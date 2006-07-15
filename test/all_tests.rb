bd6f91efb3b0ba07437ca1db13bbe9475984c5b6
James Mead <james@floehopper.org>
2006-07-15 12:59:17 +0000

    Moved stubba tests into stubba subdirectory.

--- test/all_tests.rb
+++ test/all_tests.rb
@@ -43,11 +43,11 @@ end
 
 Test::Unit::UI::Console::TestRunner.run(MochaUnitTests)
 
-require 'stubba_test'
-require 'stubba_class_method_test'
-require 'stubba_instance_method_test'
-require 'stubba_any_instance_method_test'
-require 'stubba_test_case_test'
+require 'stubba/stubba_test'
+require 'stubba/stubba_class_method_test'
+require 'stubba/stubba_instance_method_test'
+require 'stubba/stubba_any_instance_method_test'
+require 'stubba/stubba_test_case_test'
 
 class StubbaUnitTests
   
@@ -65,7 +65,7 @@ end
 
 Test::Unit::UI::Console::TestRunner.run(StubbaUnitTests)
 
-require 'stubba_object_test'
+require 'stubba/stubba_object_test'
 
 class IsolatedStubbaUnitTests # avoid loading stubba_object until now which breaks other stubba unit tests
   
@@ -79,11 +79,11 @@ end
 
 Test::Unit::UI::Console::TestRunner.run(IsolatedStubbaUnitTests)
 
-require 'stubba_integration_test'
+require 'stubba/stubba_integration_test'
 Test::Unit::UI::Console::TestRunner.run(StubbaIntegrationTest)
 
 require 'mock_acceptance_test'
-require 'stubba_acceptance_test'
+require 'stubba/stubba_acceptance_test'
 require 'auto_mock_acceptance_test'
 
 class AcceptanceTests
