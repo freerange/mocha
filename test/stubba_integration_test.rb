7e8c046dc11700b5d3c90236b95c0e26849da221
James Mead <james@floehopper.org>
2007-01-21 14:50:26 +0000

    Added tests to check that instance methods on partial mocks are restored to original implementation in case a partial mock instance is used in multiple test methods (see emails from Kevin Clark regarding tests on Rails routing code - http://rubyforge.org/pipermail/mocha-developer/2007-January/000177.html).

--- test/stubba_integration_test.rb
+++ test/stubba_integration_test.rb
@@ -7,53 +7,83 @@ require 'mocha/standalone'
 class StubbaIntegrationTest < Test::Unit::TestCase
   
   class DontMessWithMe
-    def self.method_x
+    def self.my_class_method
+      :original_return_value
+    end
+    def my_instance_method
       :original_return_value
     end
   end
   
   def test_should_stub_class_method_within_test
-    test_class = Class.new(Test::Unit::TestCase) do
-      include Mocha::Standalone
-      include Mocha::TestCaseAdapter
-      define_method(:test_me) do
-        DontMessWithMe.expects(:method_x).returns(:new_return_value)
-        assert_equal :new_return_value, DontMessWithMe.method_x
-      end
+    test = build_test do
+      DontMessWithMe.expects(:my_class_method).returns(:new_return_value)
+      assert_equal :new_return_value, DontMessWithMe.my_class_method
     end
-    test = test_class.new(:test_me)
 
     test_result = Test::Unit::TestResult.new
     test.run(test_result) {}
     assert test_result.passed?
   end
 
-  def test_should_leave_stubbed_class_unchanged_after_test
-    test_class = Class.new(Test::Unit::TestCase) do
-      include Mocha::Standalone
-      include Mocha::TestCaseAdapter
-      define_method(:test_me) do
-        DontMessWithMe.expects(:method_x).returns(:new_return_value)
-      end
+  def test_should_leave_stubbed_class_method_unchanged_after_test
+    test = build_test do
+      DontMessWithMe.expects(:my_class_method).returns(:new_return_value)
     end
-    test = test_class.new(:test_me)
 
     test.run(Test::Unit::TestResult.new) {}
-    assert_equal :original_return_value, DontMessWithMe.method_x
+    assert_equal :original_return_value, DontMessWithMe.my_class_method
   end
   
-  def test_should_reset_expectations_after_test
-    test_class = Class.new(Test::Unit::TestCase) do
-      include Mocha::Standalone
-      include Mocha::TestCaseAdapter
-      define_method(:test_me) do
-        DontMessWithMe.expects(:method_x)
-      end
+  def test_should_reset_class_expectations_after_test
+    test = build_test do
+      DontMessWithMe.expects(:my_class_method)
     end
-    test = test_class.new(:test_me)
     
     test.run(Test::Unit::TestResult.new) {}
     assert_equal 0, DontMessWithMe.mocha.expectations.size
   end  
+
+  def test_should_stub_instance_method_within_test
+    instance = DontMessWithMe.new
+    test = build_test do
+      instance.expects(:my_instance_method).returns(:new_return_value)
+      assert_equal :new_return_value, instance.my_instance_method
+    end
+    test_result = Test::Unit::TestResult.new
+    test.run(test_result) {}
+    assert test_result.passed?
+  end
+  
+  def test_should_leave_stubbed_instance_method_unchanged_after_test
+    instance = DontMessWithMe.new
+    test = build_test do
+      instance.expects(:my_instance_method).returns(:new_return_value)
+    end
+
+    test.run(Test::Unit::TestResult.new) {}
+    assert_equal :original_return_value, instance.my_instance_method
+  end
+  
+  def test_should_reset_instance_expectations_after_test
+    instance = DontMessWithMe.new
+    test = build_test do
+      instance.expects(:my_instance_method).returns(:new_return_value)
+    end
+    
+    test.run(Test::Unit::TestResult.new) {}
+    assert_equal 0, instance.mocha.expectations.size
+  end  
+
+  private
+
+  def build_test(&block)
+    test_class = Class.new(Test::Unit::TestCase) do
+      include Mocha::Standalone
+      include Mocha::TestCaseAdapter
+      define_method(:test_me, &block)
+    end
+    test_class.new(:test_me)
+  end
 
 end
