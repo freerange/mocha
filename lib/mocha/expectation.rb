47b774b17debe9a8bce89c1fcc9074a2577a69d1
James Mead <james@floehopper.org>
2007-02-02 13:02:50 +0000

    Fixed bug [#7834] infinte_range.rb makes incorrect assumption about to_f logged by James Moore.

--- lib/mocha/expectation.rb
+++ lib/mocha/expectation.rb
@@ -247,7 +247,7 @@ module Mocha
     end
     
     def error_message(expected_count, actual_count)
-      "#{@mock.mocha_inspect}.#{method_signature} - expected calls: #{expected_count}, actual calls: #{actual_count}"
+      "#{@mock.mocha_inspect}.#{method_signature} - expected calls: #{expected_count.mocha_inspect}, actual calls: #{actual_count}"
     end
   
     # :startdoc:
--- test/mocha/infinite_range_test.rb
+++ test/mocha/infinite_range_test.rb
@@ -1,5 +1,6 @@
 require File.join(File.dirname(__FILE__), "..", "test_helper")
 require 'mocha/infinite_range'
+require 'date'
 
 class InfiniteRangeTest < Test::Unit::TestCase
   
@@ -18,7 +19,7 @@ class InfiniteRangeTest < Test::Unit::TestCase
   end
   
   def test_should_be_human_readable_description_for_at_least
-    assert_equal "at least 10", Range.at_least(10).to_s
+    assert_equal "at least 10", Range.at_least(10).mocha_inspect
   end
   
   def test_should_include_values_at_or_below_maximum
@@ -35,12 +36,14 @@ class InfiniteRangeTest < Test::Unit::TestCase
   end
   
   def test_should_be_human_readable_description_for_at_most
-    assert_equal "at most 10", Range.at_most(10).to_s
+    assert_equal "at most 10", Range.at_most(10).mocha_inspect
   end
   
-  def test_should_not_break_original_description
-    assert_equal "1..10", (1..10).to_s
-    assert_equal "1...10", (1...10).to_s
+  def test_should_be_same_as_standard_to_string
+    assert_equal (1..10).to_s, (1..10).mocha_inspect
+    assert_equal (1...10).to_s, (1...10).mocha_inspect
+    date_range = Range.new(Date.parse('2006-01-01'), Date.parse('2007-01-01'))
+    assert_equal date_range.to_s, date_range.mocha_inspect
   end
   
   def assert_false(condition)
