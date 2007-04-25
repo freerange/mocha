ce37d7d884ea010884078a43acba2ab0b029cd90
James Mead <james@floehopper.org>
2007-04-25 14:34:12 +0000

    Added rdoc for any_of and all_of parameters matchers.

--- lib/mocha/parameter_matchers/any_of.rb
+++ lib/mocha/parameter_matchers/any_of.rb
@@ -3,6 +3,22 @@ module Mocha
   module ParameterMatchers
 
     # :call-seq: any_of -> parameter_matcher
+    #
+    # Matches if any +matchers+ match.
+    #   object = mock()
+    #   object.expects(:method_1).with(any_of(1, 3))
+    #   object.method_1(1)
+    #   # no error raised
+    #
+    #   object = mock()
+    #   object.expects(:method_1).with(any_of(1, 3))
+    #   object.method_1(3)
+    #   # no error raised
+    #
+    #   object = mock()
+    #   object.expects(:method_1).with(any_of(1, 3))
+    #   object.method_1(2)
+    #   # error raised, because method_1 was not called with 1 or 3
     def any_of(*matchers)
       AnyOf.new(*matchers)
     end
