56a1fb142c0d6ac45b90b727bc4b1b5bac7d4953
Paul Battley <pbattley@gmail.com>
2007-04-25 10:33:20 +0000

    Added rdoc to 'anything'.

--- lib/mocha/parameter_matchers/anything.rb
+++ lib/mocha/parameter_matchers/anything.rb
@@ -1,6 +1,13 @@
 module Mocha
   module ParameterMatchers
 
+    # :call-seq: anything -> parameter_matcher
+    #
+    # Matches any object.
+    #   object = mock()
+    #   object.expects(:method_1).with(anything)
+    #   object.method_1('foo')
+    #   # no error raised
     def anything
       Anything.new
     end
