ae74457dd0477405f68969eebc17a4ad0a82306c
James Mead <james@floehopper.org>
2007-06-30 20:40:29 +0000

    No need to reference Mock using Mocha namespace.

--- lib/mocha/auto_verify.rb
+++ lib/mocha/auto_verify.rb
@@ -92,7 +92,7 @@ module Mocha # :nodoc:
     def build_mock_with_expectations(expectation_type = :expects, expectations = {}, name = nil) # :nodoc:
       stub_everything = (expectation_type == :stub_everything)
       expectation_type = :stubs if expectation_type == :stub_everything
-      mock = Mocha::Mock.new(stub_everything, name)
+      mock = Mock.new(stub_everything, name)
       expectations.each do |method, result|
         mock.__send__(expectation_type, method).returns(result)
       end
