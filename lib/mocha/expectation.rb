c2500d253b1e47fbf27fd63e2d3cd3d24c9eef44
James Mead <james@floehopper.org>
2007-10-04 21:18:12 +0000

    Combine AnyParameters into Parameters.

--- lib/mocha/expectation.rb
+++ lib/mocha/expectation.rb
@@ -17,7 +17,7 @@ module Mocha # :nodoc:
 
     def initialize(mock, method_name, backtrace = nil)
       @mock, @method_name = mock, method_name
-      @parameters = AnyParameters.new
+      @parameters = Parameters.new
       @expected_count, @invoked_count = 1, 0
       @return_values = ReturnValues.new
       @yield_parameters = YieldParameters.new
--- lib/mocha/parameters.rb
+++ lib/mocha/parameters.rb
@@ -4,31 +4,23 @@ module Mocha
   
   class Parameters
     
-    def initialize(parameters, &block)
+    def initialize(parameters = nil, &block)
       @parameters, @block = parameters, block
     end
     
     def match?(parameters)
-      (@block && @block.call(*parameters)) || (@parameters == parameters)
+      @parameters.nil? || (@block && @block.call(*parameters)) || (@parameters == parameters)
     end
     
     def to_s
-      text = @parameters.mocha_inspect
-      text = text.gsub(/^\[|\]$/, '')
-      text = text.gsub(/^\{|\}$/, '') if @parameters.length == 1
-      "(#{text})"
-    end
-    
-  end
-
-  class AnyParameters
-    
-    def match?(parameters)
-      true
-    end
-    
-    def to_s
-      ""
+      if @parameters then
+        text = @parameters.mocha_inspect
+        text = text.gsub(/^\[|\]$/, '')
+        text = text.gsub(/^\{|\}$/, '') if @parameters.length == 1
+        "(#{text})"
+      else
+        ""
+      end
     end
     
   end
