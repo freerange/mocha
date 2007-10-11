1b7191bb44104a0f93b178072642667cb019ea26
James Mead <james@floehopper.org>
2007-10-11 15:27:05 +0000

    Simplified logic in MissingExpectation.

--- lib/mocha/missing_expectation.rb
+++ lib/mocha/missing_expectation.rb
@@ -4,17 +4,11 @@ module Mocha # :nodoc:
   
   class MissingExpectation < Expectation # :nodoc:
 
-    def initialize(mock, method_name)
-      super
-      @invoked_count = true
-    end
-
     def verify
       msg = error_message(0, 1)
       similar_expectations_list = method_signature.similar_method_signatures.join("\n")
       msg << "\nSimilar expectations:\n#{similar_expectations_list}" unless similar_expectations_list.empty?
-      error = ExpectationError.new(msg, backtrace)
-      raise error if @invoked_count
+      raise ExpectationError.new(msg, backtrace)
     end
 
   end
