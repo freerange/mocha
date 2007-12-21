0cc4ee2d287133849a55af10e52da46f8f6a6f4d
James Mead <james@floehopper.org>
2007-12-21 23:49:09 +0000

    Fixed mistake in Ruby 1.9 compatibility changeset:206 which led to errors like this one reported by Ned Wolpert... NoMethodError: undefined method `ord\' for 63:Fixnum. Would probably be better to use explicit version checking rather than respond_to? calls, but I'll sort that out later.

--- lib/mocha/class_method.rb
+++ lib/mocha/class_method.rb
@@ -42,7 +42,7 @@ module Mocha
     end
   
     def hidden_method
-      method_name = method.to_s.gsub(/\W/) { |s| "_substituted_character_#{s.respond_to?(:ord) ? s[0].ord : s[0]}_" }
+      method_name = method.to_s.gsub(/\W/) { |s| "_substituted_character_#{s[0].respond_to?(:ord) ? s[0].ord : s[0]}_" }
       "__stubba__#{method_name}__stubba__"
     end  
   
