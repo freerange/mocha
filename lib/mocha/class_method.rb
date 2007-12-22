39b8709c7b001b236f9e4c346f298a5e758938d9
James Mead <james@floehopper.org>
2007-12-22 00:08:10 +0000

    Be more explicit about changes for Ruby 1.9 compatibility.

--- lib/mocha/class_method.rb
+++ lib/mocha/class_method.rb
@@ -42,7 +42,11 @@ module Mocha
     end
   
     def hidden_method
-      method_name = method.to_s.gsub(/\W/) { |s| "_substituted_character_#{s[0].respond_to?(:ord) ? s[0].ord : s[0]}_" }
+      if RUBY_VERSION < '1.9'
+        method_name = method.to_s.gsub(/\W/) { |s| "_substituted_character_#{s[0]}_" }
+      else
+        method_name = method.to_s.gsub(/\W/) { |s| "_substituted_character_#{s.ord}_" }
+      end
       "__stubba__#{method_name}__stubba__"
     end  
   
