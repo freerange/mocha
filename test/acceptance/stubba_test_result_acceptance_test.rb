99d2fe3f028a6ca6b58ac196fb7c376102454e36
James Mead <james@floehopper.org>
2008-01-21 02:41:11 +0000

    Moved another integration test to acceptance tests and use TestRunner module.

--- test/acceptance/stubba_test_result_acceptance_test.rb
+++ test/acceptance/stubba_test_result_acceptance_test.rb
@@ -1,11 +1,12 @@
 require File.join(File.dirname(__FILE__), "..", "test_helper")
-require 'mocha/object'
-require 'mocha/standalone'
-require 'mocha/test_case_adapter'
+require 'mocha'
+require 'test_runner'
 require 'execution_point'
 
-class StubbaTestResultIntegrationTest < Test::Unit::TestCase
-
+class StubbaTestResultAcceptanceTest < Test::Unit::TestCase
+  
+  include TestRunner
+  
   def test_should_include_expectation_verification_in_assertion_count
     test_result = run_test do
       object = Class.new { def message; end }.new
@@ -47,39 +48,22 @@ class StubbaTestResultIntegrationTest < Test::Unit::TestCase
   end
   
   def test_should_display_backtrace_indicating_line_number_where_expects_was_called
-    test_result = Test::Unit::TestResult.new
-    faults = []
-    test_result.add_listener(Test::Unit::TestResult::FAULT, &lambda { |fault| faults << fault })
     execution_point = nil
-    run_test(test_result) do
+    test_result = run_test do
       object = Class.new { def message; end }.new
       execution_point = ExecutionPoint.current; object.expects(:message)
     end
-    assert_equal 1, faults.length
-    assert_equal execution_point, ExecutionPoint.new(faults.first.location)
+    assert_equal 1, test_result.failure_count
+    assert_equal execution_point, ExecutionPoint.new(test_result.failures[0].location)
   end
   
   def test_should_display_backtrace_indicating_line_number_where_failing_assertion_was_called
-    test_result = Test::Unit::TestResult.new
-    faults = []
-    test_result.add_listener(Test::Unit::TestResult::FAULT, &lambda { |fault| faults << fault })
     execution_point = nil
-    run_test(test_result) do
+    test_result = run_test do
       execution_point = ExecutionPoint.current; flunk
     end
-    assert_equal 1, faults.length
-    assert_equal execution_point, ExecutionPoint.new(faults.first.location)
-  end
-  
-  def run_test(test_result = Test::Unit::TestResult.new, &block)
-    test_class = Class.new(Test::Unit::TestCase) do
-      include Mocha::Standalone
-      include Mocha::TestCaseAdapter
-      define_method(:test_me, &block)
-    end
-    test = test_class.new(:test_me)
-    test.run(test_result) {}
-    test_result
+    assert_equal 1, test_result.failure_count
+    assert_equal execution_point, ExecutionPoint.new(test_result.failures[0].location)
   end
   
 end
