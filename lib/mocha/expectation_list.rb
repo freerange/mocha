887e7bc94b44c4071a5aefb6c8e17e30a1c2937b
James Mead <james@floehopper.org>
2008-02-17 13:57:02 +0000

    ExpectationList#similar_expectations is no longer used.

--- lib/mocha/expectation_list.rb
+++ lib/mocha/expectation_list.rb
@@ -5,38 +5,34 @@ module Mocha # :nodoc:
     def initialize
       @expectations = []
     end
-
+    
     def add(expectation)
       @expectations << expectation
       expectation
     end
-
+    
     def matches_method?(method_name)
       @expectations.any? { |expectation| expectation.matches_method?(method_name) }
     end
-
-    def similar(method_name)
-      @expectations.select { |expectation| expectation.matches_method?(method_name) }
-    end
-
+    
     def detect(method_name, *arguments)
       expectations = @expectations.reverse.select { |e| e.match?(method_name, *arguments) }
       expectation = expectations.detect { |e| e.invocations_allowed? }
       expectation || expectations.first
     end
-
+    
     def verified?(assertion_counter = nil)
       @expectations.all? { |expectation| expectation.verified?(assertion_counter) }
     end
-
+    
     def to_a
       @expectations
     end
-
+    
     def to_set
       @expectations.to_set
     end
-
+    
     def length
       @expectations.length
     end
