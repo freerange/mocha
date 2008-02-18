c24b5bb9f3853f58f4f54342151b4b7e54a16856
James Mead <james@floehopper.org>
2008-02-18 21:26:51 +0000

    Make ExpectationRaiser deal with subclasses of Interrupt which seem to need a message supplied in the raise statement in Ruby 1.8.6 (but not 1.8.4 or 1.9). Not sure this is really Mocha's responsibility.

--- test/unit/exception_raiser_test.rb
+++ test/unit/exception_raiser_test.rb
@@ -1,6 +1,7 @@
 require File.join(File.dirname(__FILE__), "..", "test_helper")
 
 require 'mocha/exception_raiser'
+require 'timeout'
 
 class ExceptionRaiserTest < Test::Unit::TestCase
   
@@ -20,9 +21,22 @@ class ExceptionRaiserTest < Test::Unit::TestCase
     assert_equal 'message', exception.message
   end
   
+  def test_should_raise_exception_instance
+    exception_class = Class.new(StandardError)
+    raiser = ExceptionRaiser.new(exception_class.new('message'), nil)
+    exception = assert_raises(exception_class) { raiser.evaluate }
+    assert_equal 'message', exception.message
+  end
+  
   def test_should_raise_interrupt_exception_with_default_message_so_it_works_in_ruby_1_8_6
     raiser = ExceptionRaiser.new(Interrupt, nil)
     assert_raises(Interrupt) { raiser.evaluate }
+  end
+
+  def test_should_raise_subclass_of_interrupt_exception_with_default_message_so_it_works_in_ruby_1_8_6
+    exception_class = Class.new(Interrupt)
+    raiser = ExceptionRaiser.new(exception_class, nil)
+    assert_raises(exception_class) { raiser.evaluate }
   end
 
 end
