c544cc8952ba98471f828d3e08aa12d213acc8bf
James Mead <james@floehopper.org>
2008-02-22 11:46:43 +0000

    Removed unused lines from Mockery#verify.

--- lib/mocha/mockery.rb
+++ lib/mocha/mockery.rb
@@ -41,9 +41,6 @@ module Mocha
     
     def verify(assertion_counter = nil)
       unless mocks.all? { |mock| mock.verified?(assertion_counter) }
-        expectations = mocks.map { |mock| mock.expectations.to_a }.flatten
-        unsatisfied = expectations.reject { |e| e.verified? }
-        satisfied = expectations.select { |e| e.verified? }
         message = "not all expectations were satisfied\n#{mocha_inspect}"
         if unsatisfied_expectations.empty?
           backtrace = caller
