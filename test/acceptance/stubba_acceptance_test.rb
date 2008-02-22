37b5ad7604152dc8e23eacfd0f3d61bb355d962d
James Mead <james@floehopper.org>
2008-02-22 17:42:02 +0000

    Make StubbaAcceptanceTests more self-contained.

--- test/acceptance/stubba_acceptance_test.rb
+++ test/acceptance/stubba_acceptance_test.rb
@@ -4,69 +4,60 @@ require 'test_runner'
 
 class StubbaAcceptanceTest < Test::Unit::TestCase
   
-  class DontMessWithMe
-    def self.my_class_method
-      :original_return_value
-    end
-    def my_instance_method
-      :original_return_value
-    end
-  end
-  
-  module LeaveMeAlone
-    def self.my_module_method
-      :original_return_value
-    end
-  end
-  
   include TestRunner
   
   def test_should_stub_class_method_within_test
+    klass = Class.new { def self.my_class_method; :original_return_value; end }
     test_result = run_test do
-      DontMessWithMe.stubs(:my_class_method).returns(:new_return_value)
-      assert_equal :new_return_value, DontMessWithMe.my_class_method
+      klass.stubs(:my_class_method).returns(:new_return_value)
+      assert_equal :new_return_value, klass.my_class_method
     end
     assert_passed(test_result)
   end
 
   def test_should_leave_stubbed_class_method_unchanged_after_test
+    klass = Class.new { def self.my_class_method; :original_return_value; end }
     run_test do
-      DontMessWithMe.stubs(:my_class_method).returns(:new_return_value)
+      klass.stubs(:my_class_method).returns(:new_return_value)
     end
-    assert_equal :original_return_value, DontMessWithMe.my_class_method
+    assert_equal :original_return_value, klass.my_class_method
   end
   
   def test_should_reset_class_expectations_after_test
+    klass = Class.new { def self.my_class_method; :original_return_value; end }
     run_test do
-      DontMessWithMe.stubs(:my_class_method)
+      klass.stubs(:my_class_method)
     end
-    assert_equal 0, DontMessWithMe.mocha.expectations.length
+    assert_equal 0, klass.mocha.expectations.length
   end  
 
   def test_should_stub_module_method_within_test
+    mod = Module.new { def self.my_module_method; :original_return_value; end }
     test_result = run_test do
-      LeaveMeAlone.stubs(:my_module_method).returns(:new_return_value)
-      assert_equal :new_return_value, LeaveMeAlone.my_module_method
+      mod.stubs(:my_module_method).returns(:new_return_value)
+      assert_equal :new_return_value, mod.my_module_method
     end
     assert_passed(test_result)
   end
 
   def test_should_leave_stubbed_module_method_unchanged_after_test
+    mod = Module.new { def self.my_module_method; :original_return_value; end }
     run_test do
-      LeaveMeAlone.stubs(:my_module_method).returns(:new_return_value)
+      mod.stubs(:my_module_method).returns(:new_return_value)
     end
-    assert_equal :original_return_value, LeaveMeAlone.my_module_method
+    assert_equal :original_return_value, mod.my_module_method
   end
   
   def test_should_reset_module_expectations_after_test
+    mod = Module.new { def self.my_module_method; :original_return_value; end }
     run_test do
-      LeaveMeAlone.stubs(:my_module_method)
+      mod.stubs(:my_module_method)
     end
-    assert_equal 0, LeaveMeAlone.mocha.expectations.length
+    assert_equal 0, mod.mocha.expectations.length
   end  
 
   def test_should_stub_instance_method_within_test
-    instance = DontMessWithMe.new
+    instance = Class.new { def my_instance_method; :original_return_value; end }.new
     test_result = run_test do
       instance.stubs(:my_instance_method).returns(:new_return_value)
       assert_equal :new_return_value, instance.my_instance_method
@@ -75,7 +66,7 @@ class StubbaAcceptanceTest < Test::Unit::TestCase
   end
   
   def test_should_leave_stubbed_instance_method_unchanged_after_test
-    instance = DontMessWithMe.new
+    instance = Class.new { def my_instance_method; :original_return_value; end }.new
     run_test do
       instance.stubs(:my_instance_method).returns(:new_return_value)
     end
@@ -83,7 +74,7 @@ class StubbaAcceptanceTest < Test::Unit::TestCase
   end
   
   def test_should_reset_instance_expectations_after_test
-    instance = DontMessWithMe.new
+    instance = Class.new { def my_instance_method; :original_return_value; end }.new
     run_test do
       instance.stubs(:my_instance_method).returns(:new_return_value)
     end
@@ -91,28 +82,31 @@ class StubbaAcceptanceTest < Test::Unit::TestCase
   end  
 
   def test_should_stub_any_instance_method_within_test
-    instance = DontMessWithMe.new
+    klass = Class.new { def my_instance_method; :original_return_value; end }
+    instance = klass.new
     test_result = run_test do
-      DontMessWithMe.any_instance.stubs(:my_instance_method).returns(:new_return_value)
+      klass.any_instance.stubs(:my_instance_method).returns(:new_return_value)
       assert_equal :new_return_value, instance.my_instance_method
     end
     assert_passed(test_result)
   end
   
   def test_should_leave_stubbed_any_instance_method_unchanged_after_test
-    instance = DontMessWithMe.new
+    klass = Class.new { def my_instance_method; :original_return_value; end }
+    instance = klass.new
     run_test do
-      DontMessWithMe.any_instance.stubs(:my_instance_method).returns(:new_return_value)
+      klass.any_instance.stubs(:my_instance_method).returns(:new_return_value)
     end
     assert_equal :original_return_value, instance.my_instance_method
   end
   
   def test_should_reset_any_instance_expectations_after_test
-    instance = DontMessWithMe.new
+    klass = Class.new { def my_instance_method; :original_return_value; end }
+    instance = klass.new
     run_test do
-      DontMessWithMe.any_instance.stubs(:my_instance_method).returns(:new_return_value)
+      klass.any_instance.stubs(:my_instance_method).returns(:new_return_value)
     end
-    assert_equal 0, DontMessWithMe.any_instance.mocha.expectations.length
+    assert_equal 0, klass.any_instance.mocha.expectations.length
   end  
   
 end
