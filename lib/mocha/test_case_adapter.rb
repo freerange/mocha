917e23bbd24b269575f9a0f4b42279617cba14f0
James Mead <james@floehopper.org>
2008-03-09 15:10:40 +0000

    Nasty fix so that TestCaseAdaptor works consistently with earlier versions of Test::Unit as well as more recent versions.

--- lib/mocha/test_case_adapter.rb
+++ lib/mocha/test_case_adapter.rb
@@ -17,43 +17,83 @@ module Mocha
     end
 
     def self.included(base)
-      base.class_eval do
+      if RUBY_VERSION < '1.8.6'
+        base.class_eval do
 
-        alias_method :run_before_mocha_test_case_adapter, :run
+          alias_method :run_before_mocha_test_case_adapter, :run
 
-        def run(result)
-          assertion_counter = AssertionCounter.new(result)
-          yield(Test::Unit::TestCase::STARTED, name)
-          @_result = result
-          begin
+          def run(result)
+            assertion_counter = AssertionCounter.new(result)
+            yield(Test::Unit::TestCase::STARTED, name)
+            @_result = result
             begin
-              setup
-              __send__(@method_name)
-              mocha_verify(assertion_counter)
-            rescue Mocha::ExpectationError => e
-              add_failure(e.message, e.backtrace)
-            rescue Test::Unit::AssertionFailedError => e
-              add_failure(e.message, e.backtrace)
-            rescue Exception
-              raise if Test::Unit::TestCase::PASSTHROUGH_EXCEPTIONS.include? $!.class
-              add_error($!)
-            ensure
               begin
-                teardown
+                setup
+                __send__(@method_name)
+                mocha_verify(assertion_counter)
+              rescue Mocha::ExpectationError => e
+                add_failure(e.message, e.backtrace)
+              rescue Test::Unit::AssertionFailedError => e
+                add_failure(e.message, e.backtrace)
+              rescue StandardError, ScriptError
+                add_error($!)
+              ensure
+                begin
+                  teardown
+                rescue Test::Unit::AssertionFailedError => e
+                  add_failure(e.message, e.backtrace)
+                rescue StandardError, ScriptError
+                  add_error($!)
+                end
+              end
+            ensure
+              mocha_teardown
+            end
+            result.add_run
+            yield(Test::Unit::TestCase::FINISHED, name)
+          end
+
+        end
+      else
+        base.class_eval do
+
+          alias_method :run_before_mocha_test_case_adapter, :run
+
+          def run(result)
+            assertion_counter = AssertionCounter.new(result)
+            yield(Test::Unit::TestCase::STARTED, name)
+            @_result = result
+            begin
+              begin
+                setup
+                __send__(@method_name)
+                mocha_verify(assertion_counter)
+              rescue Mocha::ExpectationError => e
+                add_failure(e.message, e.backtrace)
               rescue Test::Unit::AssertionFailedError => e
                 add_failure(e.message, e.backtrace)
               rescue Exception
                 raise if Test::Unit::TestCase::PASSTHROUGH_EXCEPTIONS.include? $!.class
                 add_error($!)
+              ensure
+                begin
+                  teardown
+                rescue Test::Unit::AssertionFailedError => e
+                  add_failure(e.message, e.backtrace)
+                rescue Exception
+                  raise if Test::Unit::TestCase::PASSTHROUGH_EXCEPTIONS.include? $!.class
+                  add_error($!)
+                end
               end
+            ensure
+              mocha_teardown
             end
-          ensure
-            mocha_teardown
+            result.add_run
+            yield(Test::Unit::TestCase::FINISHED, name)
           end
-          result.add_run
-          yield(Test::Unit::TestCase::FINISHED, name)
-        end
                 
+        end
+        
       end
       
     end
