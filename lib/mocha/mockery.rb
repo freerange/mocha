35ee18288f0bc8a0d5d3b66908640c4c9670610b
James Mead <james@floehopper.org>
2008-03-10 13:06:14 +0000

    Added hyphens when listing unsatisfied expectations, satisfied expectations and states, so that the regular expression in TextMate's Ruby bundle run_script.rb doesn't mistake a Mocha error message for a stack trace line.

--- lib/mocha/mockery.rb
+++ lib/mocha/mockery.rb
@@ -76,9 +76,9 @@ module Mocha
     
     def mocha_inspect
       message = ""
-      message << "unsatisfied expectations:\n  #{unsatisfied_expectations.map { |e| e.mocha_inspect }.join("\n  ")}\n" unless unsatisfied_expectations.empty?
-      message << "satisfied expectations:\n  #{satisfied_expectations.map { |e| e.mocha_inspect }.join("\n  ")}\n" unless satisfied_expectations.empty?
-      message << "states:\n  #{state_machines.map { |sm| sm.mocha_inspect }.join("\n  ")}" unless state_machines.empty?
+      message << "unsatisfied expectations:\n- #{unsatisfied_expectations.map { |e| e.mocha_inspect }.join("\n- ")}\n" unless unsatisfied_expectations.empty?
+      message << "satisfied expectations:\n- #{satisfied_expectations.map { |e| e.mocha_inspect }.join("\n- ")}\n" unless satisfied_expectations.empty?
+      message << "states:\n- #{state_machines.map { |sm| sm.mocha_inspect }.join("\n- ")}" unless state_machines.empty?
       message
     end
     
