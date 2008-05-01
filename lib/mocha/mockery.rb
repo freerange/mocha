689b0f911b42131870f42e1550a1660d5c0fafab
James Mead <james@floehopper.org>
2008-05-01 16:41:44 +0000

    Give StubbingException a sensible backtrace at least for stubbing method unnecessarily.

--- lib/mocha/mockery.rb
+++ lib/mocha/mockery.rb
@@ -104,7 +104,9 @@ module Mocha
     end
     
     def on_stubbing_method_unnecessarily(expectation)
-      raise StubbingError, "stubbing method unnecessarily: #{expectation.method_signature}" if Mocha::Configuration.prevent?(:stubbing_method_unnecessarily)
+      stubbing_error = StubbingError.new("stubbing method unnecessarily: #{expectation.method_signature}")
+      stubbing_error.set_backtrace(expectation.backtrace)
+      raise stubbing_error if Mocha::Configuration.prevent?(:stubbing_method_unnecessarily)
       logger.warn "stubbing method unnecessarily: #{expectation.method_signature}" if Mocha::Configuration.warn_when?(:stubbing_method_unnecessarily)
     end
     
