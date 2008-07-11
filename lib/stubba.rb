cef69c2221e8c7dbe4c864ea605c2afa377e7a25
James Mead <james@floehopper.org>
2008-07-11 10:17:31 +0000

    Fix for bug #21161 - 'uninitialized constant Deprecation in stubba.rb'.

--- lib/stubba.rb
+++ lib/stubba.rb
@@ -1,3 +1,4 @@
 # for backwards compatibility
 require 'mocha'
-Deprecation.warning "require 'stubba' is no longer needed and stubba.rb will soon be removed"
+require 'mocha/deprecation'
+Mocha::Deprecation.warning "require 'stubba' is no longer needed and stubba.rb will soon be removed"
