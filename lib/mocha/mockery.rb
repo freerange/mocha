caba5449f6872d7604e2f210523334369c1a1e69
James Mead <james@floehopper.org>
2008-07-17 13:03:22 +0000

    There is no point in running (potentially expensive) checks if configuration is set to allow such checks to fail. This is a relatively quick fix in response to Chris McGrath's performance problems. In the long run I suspect there is some duplication here that could be refactored away.

--- lib/mocha/mockery.rb
+++ lib/mocha/mockery.rb
@@ -53,7 +53,11 @@ module Mocha
         raise ExpectationError.new(message, backtrace)
       end
       expectations.each do |e|
-        on_stubbing_method_unnecessarily(e) unless e.used?
+        unless Mocha::Configuration.allow?(:stubbing_method_unnecessarily)
+          unless e.used?
+            on_stubbing_method_unnecessarily(e)
+          end
+        end
       end
     end
     
@@ -83,9 +87,19 @@ module Mocha
     end
     
     def on_stubbing(object, symbol)
-      on_stubbing_non_existent_method(object, symbol) unless object.method_exists?(symbol, include_public_methods = true)
-      on_stubbing_non_public_method(object, symbol) if object.method_exists?(symbol, include_public_methods = false)
-      on_stubbing_method_on_non_mock_object(object, symbol)
+      unless Mocha::Configuration.allow?(:stubbing_non_existent_method)
+        unless object.method_exists?(symbol, include_public_methods = true)
+          on_stubbing_non_existent_method(object, symbol)
+        end
+      end
+      unless Mocha::Configuration.allow?(:stubbing_non_public_method)
+        if object.method_exists?(symbol, include_public_methods = false)
+          on_stubbing_non_public_method(object, symbol)
+        end
+      end
+      unless Mocha::Configuration.allow?(:stubbing_method_on_non_mock_object)
+        on_stubbing_method_on_non_mock_object(object, symbol)
+      end
     end
     
     def on_stubbing_non_existent_method(object, symbol)
