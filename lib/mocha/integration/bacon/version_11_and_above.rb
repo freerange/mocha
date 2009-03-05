82ea1493fe818132689583dc44ec554e7440e14d
James Mead <james@floehopper.org>
2009-03-05 16:05:50 +0000

    Fixed bug: Some Bacon tests were failing in Ruby 1.9.1. http://floehopper.lighthouseapp.com/projects/22289-mocha/tickets/43.

--- lib/mocha/integration/bacon/version_11_and_above.rb
+++ lib/mocha/integration/bacon/version_11_and_above.rb
@@ -14,7 +14,7 @@ module Mocha
             alias :it_before_bacon :it
             def it(description)
               it_before_bacon(description) do
-                assertion_counter = AssertionCounter.new(::Bacon::Counter)
+                assertion_counter = ::Mocha::Integration::Bacon::AssertionCounter.new(::Bacon::Counter)
                 mocha_setup
                 yield
                 mocha_verify(assertion_counter)
