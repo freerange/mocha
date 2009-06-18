52de7942ded7a246de55b8d9c163cbb22ecd30eb
Denis Defreyne <denis.defreyne@stoneship.org>
2009-06-18 17:58:54 +0800

    made mocha compatible with minitest 1.4.0
    Signed-off-by: James Mead <james@floehopper.org>

--- lib/mocha/integration/mini_test/version_131_and_above.rb
+++ lib/mocha/integration/mini_test/version_131_and_above.rb
@@ -18,21 +18,22 @@ module Mocha
         def run runner
           assertion_counter = AssertionCounter.new(self)
           result = '.'
+          name = (self.respond_to?(:name) ? self.name : self.__name__)
           begin
             begin
               @passed = nil
               self.setup
-              self.__send__ self.name
+              self.__send__ name
               mocha_verify(assertion_counter)
               @passed = true
             rescue Exception => e
               @passed = false
-              result = runner.puke(self.class, self.name, Mocha::Integration::MiniTest.translate(e))
+              result = runner.puke(self.class, name, Mocha::Integration::MiniTest.translate(e))
             ensure
               begin
                 self.teardown
               rescue Exception => e
-                result = runner.puke(self.class, self.name, Mocha::Integration::MiniTest.translate(e))
+                result = runner.puke(self.class, name, Mocha::Integration::MiniTest.translate(e))
               end
             end
           ensure
