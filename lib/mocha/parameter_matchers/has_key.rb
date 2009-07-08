396a533a74a5391b2032123f72cc885b9101d686
Sander Hartlage <sander@outside.in>
2009-07-09 00:00:04 +0800

    Now the has_key matcher will not raise an error on empty arguments or arguments that don't respond to #keys, nor will it match
    Signed-off-by: James Mead <james@floehopper.org>

--- lib/mocha/parameter_matchers/has_key.rb
+++ lib/mocha/parameter_matchers/has_key.rb
@@ -28,6 +28,7 @@ module Mocha
       
       def matches?(available_parameters)
         parameter = available_parameters.shift
+        return false unless parameter.respond_to?(:keys)
         parameter.keys.any? { |key| @key.to_matcher.matches?([key]) }
       end
       
--- test/unit/parameter_matchers/has_key_test.rb
+++ test/unit/parameter_matchers/has_key_test.rb
@@ -33,4 +33,23 @@ class HasKeyTest < Test::Unit::TestCase
     assert !matcher.matches?([{ :key_2 => 2 }])
   end
   
+  def test_should_not_raise_error_on_empty_arguments
+    matcher = has_key(:key)
+    assert_nothing_raised { matcher.matches?([]) }
+  end
+  
+  def test_should_not_match_on_empty_arguments
+    matcher = has_key(:key)
+    assert !matcher.matches?([])
+  end
+  
+  def test_should_not_raise_error_on_argument_that_does_not_respond_to_keys
+    matcher = has_key(:key)
+    assert_nothing_raised { matcher.matches?([:key]) }
+  end
+  
+  def test_should_not_match_on_argument_that_does_not_respond_to_keys
+    matcher = has_key(:key)
+    assert !matcher.matches?([:key])
+  end
 end
