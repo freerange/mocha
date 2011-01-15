ef4439904bb8476a661ec184a406e7f19714642e
James Mead <james@floehopper.org>
2011-01-15 10:30:00 +0000

    Added explicit support for minitest v1.5.0 to v2.0.2. Also don't monkey-patch versions we don't know about c.f. Test::Unit integration.

--- lib/mocha/integration/mini_test/version_200.rb
+++ lib/mocha/integration/mini_test/version_200.rb
@@ -7,38 +7,39 @@ module Mocha
     
     module MiniTest
       
-      module Version142AndAbove
+      module Version200
         def self.included(mod)
-          $stderr.puts "Monkey patching MiniTest >= v1.4.2" if $options['debug']
+          $stderr.puts "Monkey patching MiniTest v2.0.0" if $options['debug']
         end
         def run runner
           trap 'INFO' do
-            warn '%s#%s %.2fs' % [self.class, self.__name__,
-              (Time.now - runner.start_time)]
+            time = Time.now - runner.start_time
+            warn "%s#%s %.2fs" % [self.class, self.__name__, time]
             runner.status $stderr
           end if ::MiniTest::Unit::TestCase::SUPPORTS_INFO_SIGNAL
           
           assertion_counter = AssertionCounter.new(self)
-          result = '.'
+          result = ""
           begin
             begin
               @passed = nil
               self.setup
               self.__send__ self.__name__
               mocha_verify(assertion_counter)
+              result = "." unless io?
               @passed = true
             rescue *::MiniTest::Unit::TestCase::PASSTHROUGH_EXCEPTIONS
               raise
             rescue Exception => e
               @passed = false
-              result = runner.puke(self.class, self.__name__, Mocha::Integration::MiniTest.translate(e))
+              result = runner.puke self.class, self.__name__, Mocha::Integration::MiniTest.translate(e)
             ensure
               begin
                 self.teardown
               rescue *::MiniTest::Unit::TestCase::PASSTHROUGH_EXCEPTIONS
                 raise
               rescue Exception => e
-                result = runner.puke(self.class, self.__name__, Mocha::Integration::MiniTest.translate(e))
+                result = runner.puke self.class, self.__name__, Mocha::Integration::MiniTest.translate(e)
               end
               trap 'INFO', 'DEFAULT' if ::MiniTest::Unit::TestCase::SUPPORTS_INFO_SIGNAL
             end
