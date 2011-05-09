f1d182ea33fe08cbc187df90f12013814ae59da8
James Mead <james@floehopper.org>
2011-05-09 17:15:30 +0100

    Since the singleton method added by Mocha masks the underlying instance method, there's no need to move it out the way and then back again.
    Note that this fixes Github issue #20, because the original method is left unchanged.
    
    However, there are other possible issues that this brings to light e.g. what happens if you stub a method that is already defined as a singleton method, but that's for a future commit.

--- lib/mocha/instance_method.rb
+++ lib/mocha/instance_method.rb
@@ -4,6 +4,14 @@ module Mocha
 
   class InstanceMethod < ClassMethod
 
+    def hide_original_method
+      # intentionally left blank
+    end
+
+    def restore_original_method
+      # intentionally left blank
+    end
+
     def method_exists?(method)
       return true if stubbee.public_methods(false).include?(method)
       return true if stubbee.protected_methods(false).include?(method)
