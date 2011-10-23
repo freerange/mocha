ff4e2589a45999f6ced2a59ed7616fe9137fac73
Ryan W Sims <rwsims@gmail.com>
2011-10-23 11:08:19 -0400

    Added warning on require order.

--- lib/mocha.rb
+++ lib/mocha.rb
@@ -1,3 +1,15 @@
+def test_unit_required
+    defined?(Test) && defined?(Test::Unit) && defined?(Test::Unit::TestCase)
+end
+
+def minitest_required
+    defined?(MiniTest) && defined?(MiniTest::Unit) && defined?(MiniTest::Unit::TestCase)
+end
+
+unless test_unit_required || minitest_required
+    raise RuntimeError, "Must require mocha *after* requring a test framework."
+end
+
 require 'mocha/version'
 require 'mocha_standalone'
 require 'mocha/configuration'
