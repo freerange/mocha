8d07ed2e0af4963d1f678ad112b82a222b886d48
Steven Xu <steven@stevenxu.ca>
2011-12-27 09:28:53 -0500

    adding ordering to the tests (assuming they run in alphabetical order)

--- test/acceptance/stubbing_superclass_does_not_affect_subclass_across_test_runs_test.rb
+++ test/acceptance/stubbing_superclass_does_not_affect_subclass_across_test_runs_test.rb
@@ -17,17 +17,26 @@ class StubbingSuperclassDoesNotAffectSubclassAcrossTestRunsTest < Test::Unit::Te
     @subclass = Class.new(@superclass)
   end
 
-  def test_stubbing_superclass
+  def test_a_no_stubs_returns_expected_values_without_exceptions_before_test_that_stubs_superclass
+    puts 'before'
+    assert_nothing_raised do
+      assert_equal :unstubbed_return, @superclass.method_of_interest
+      assert_equal :unstubbed_return, @subclass.method_of_interest
+    end
+  end
+
+  def test_b_stubbing_superclass
+    puts 'stubbing'
     @superclass.stubs(:method_of_interest).returns(:stubbed_return)
+    @subclass.stubs(:method_of_interest).returns(:stubbed_return)
     assert_equal :stubbed_return, @superclass.method_of_interest
     assert_equal :stubbed_return, @subclass.method_of_interest
   end
 
-  # IMPORTANT: this test is meant to be run chronologically after the above test.
-  # TODO: I'm not 100% sure how to enforce test ordering.
-  def test_no_stubs_returns_expected_values_without_exceptions
-    assert_equal :unstubbed_return, @superclass.method_of_interest
+  def test_c_no_stubs_returns_expected_values_without_exceptions_after_test_that_stubs_superclass
+    puts 'after'
     assert_nothing_raised do
+      assert_equal :unstubbed_return, @superclass.method_of_interest
       assert_equal :unstubbed_return, @subclass.method_of_interest
     end
   end
