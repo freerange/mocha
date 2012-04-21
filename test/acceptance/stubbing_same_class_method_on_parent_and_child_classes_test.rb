d3e8fbe2e7687f5c89523546bbc67e43b0f0c168
James Mead <james@floehopper.org>
2012-04-21 11:42:10 +0100

    It's better to name tests after the behaviour they are testing.

--- test/acceptance/stubbing_same_class_method_on_parent_and_child_classes_test.rb
+++ test/acceptance/stubbing_same_class_method_on_parent_and_child_classes_test.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../acceptance_test_helper', __FILE__)
 require 'mocha'
 
-class Issue74Test < Test::Unit::TestCase
+class StubbingSameClassMethodOnParentAndChildClassTest < Test::Unit::TestCase
 
   include AcceptanceTest
 
