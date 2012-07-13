9c7dde27d0299895f4346400194de6961406f4db
James Mead <james@floehopper.org>
2012-07-13 15:26:12 +0100

    Improve the API acceptance test.

--- test/acceptance/api_test.rb
+++ test/acceptance/api_test.rb
@@ -44,40 +44,40 @@ end
 
 class SampleTest < NotATestUnitTestCase
 
-  def mocha_with_fulfilled_expectation
+  def mock_object_with_fulfilled_expectation
     mockee = mock()
     mockee.expects(:blah)
     mockee.blah
   end
 
-  def mocha_with_unfulfilled_expectation
+  def mock_object_with_unfulfilled_expectation
     mockee = mock()
     mockee.expects(:blah)
   end
 
-  def mocha_with_unexpected_invocation
+  def mock_object_with_unexpected_invocation
     mockee = mock()
     mockee.blah
   end
 
-  def stubba_with_fulfilled_expectation
+  def real_object_with_fulfilled_expectation
     stubbee = Class.new { define_method(:blah) {} }.new
     stubbee.expects(:blah)
     stubbee.blah
   end
 
-  def stubba_with_unfulfilled_expectation
+  def real_object_with_unfulfilled_expectation
     stubbee = Class.new { define_method(:blah) {} }.new
     stubbee.expects(:blah)
   end
 
-  def mocha_with_matching_parameter
+  def mock_object_with_matching_parameter
     mockee = mock()
     mockee.expects(:blah).with(has_key(:wibble))
     mockee.blah(:wibble => 1)
   end
 
-  def mocha_with_non_matching_parameter
+  def mock_object_with_non_matching_parameter
     mockee = mock()
     mockee.expects(:blah).with(has_key(:wibble))
     mockee.blah(:wobble => 2)
@@ -102,38 +102,38 @@ class APITest < Test::Unit::TestCase
     teardown_acceptance_test
   end
 
-  def test_should_pass_mocha_test
-    assert_nothing_raised { sample_test.run(:mocha_with_fulfilled_expectation) }
+  def test_should_pass_mock_object_test
+    assert_nothing_raised { sample_test.run(:mock_object_with_fulfilled_expectation) }
     assert_equal 1, sample_test.assertion_counter.count
   end
 
-  def test_should_fail_mocha_test_due_to_unfulfilled_exception
-    assert_raises(NotATestUnitAssertionFailedError) { sample_test.run(:mocha_with_unfulfilled_expectation) }
+  def test_should_fail_mock_object_test_due_to_unfulfilled_exception
+    assert_raises(NotATestUnitAssertionFailedError) { sample_test.run(:mock_object_with_unfulfilled_expectation) }
     assert_equal 1, sample_test.assertion_counter.count
   end
 
-  def test_should_fail_mocha_test_due_to_unexpected_invocation
-    assert_raises(NotATestUnitAssertionFailedError) { sample_test.run(:mocha_with_unexpected_invocation) }
+  def test_should_fail_mock_object_test_due_to_unexpected_invocation
+    assert_raises(NotATestUnitAssertionFailedError) { sample_test.run(:mock_object_with_unexpected_invocation) }
     assert_equal 0, sample_test.assertion_counter.count
   end
 
-  def test_should_pass_stubba_test
-    assert_nothing_raised { sample_test.run(:stubba_with_fulfilled_expectation) }
+  def test_should_pass_real_object_test
+    assert_nothing_raised { sample_test.run(:real_object_with_fulfilled_expectation) }
     assert_equal 1, sample_test.assertion_counter.count
   end
 
-  def test_should_fail_stubba_test
-    assert_raises(NotATestUnitAssertionFailedError) { sample_test.run(:stubba_with_unfulfilled_expectation) }
+  def test_should_fail_real_object_test
+    assert_raises(NotATestUnitAssertionFailedError) { sample_test.run(:real_object_with_unfulfilled_expectation) }
     assert_equal 1, sample_test.assertion_counter.count
   end
 
-  def test_should_pass_mocha_test_with_matching_parameter
-    assert_nothing_raised { sample_test.run(:mocha_with_matching_parameter) }
+  def test_should_pass_mock_object_test_with_matching_parameter
+    assert_nothing_raised { sample_test.run(:mock_object_with_matching_parameter) }
     assert_equal 1, sample_test.assertion_counter.count
   end
 
-  def test_should_fail_mocha_test_with_non_matching_parameter
-    assert_raises(NotATestUnitAssertionFailedError) { sample_test.run(:mocha_with_non_matching_parameter) }
+  def test_should_fail_mock_object_test_with_non_matching_parameter
+    assert_raises(NotATestUnitAssertionFailedError) { sample_test.run(:mock_object_with_non_matching_parameter) }
   end
 
 end
