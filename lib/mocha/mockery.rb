6da0c066b04c862e0229092c049541c9631ea285
Anton Shemerey <shemerey@gmail.com>
2012-07-20 18:38:50 +0300

    fix incorrect handling method all? for example [].all?

--- lib/mocha/mockery.rb
+++ lib/mocha/mockery.rb
@@ -44,7 +44,7 @@ module Mocha
     end
 
     def verify(assertion_counter = nil)
-      unless mocks.all? { |mock| mock.__verified__?(assertion_counter) }
+      if mocks.any? { |mock| !mock.__verified__?(assertion_counter) }
         message = "not all expectations were satisfied\n#{mocha_inspect}"
         if unsatisfied_expectations.empty?
           backtrace = caller
