8d37bbd39e796d71e18c211e48958fdecea0cb3a
James Mead <james@floehopper.org>
2012-08-06 12:38:43 +0100

    Keep #95 fix for Ruby 1.9, but avoid regression in Ruby 1.8.
    The clumsy code is needed to avoid a regression of #76 in Ruby 1.8.

--- lib/mocha/class_method.rb
+++ lib/mocha/class_method.rb
@@ -66,7 +66,14 @@ module Mocha
 
     def restore_original_method
       if @original_method && @original_method.owner == stubbee.__metaclass__
-        stubbee.__metaclass__.send(:define_method, method, @original_method)
+        if RUBY_VERSION < '1.9'
+          original_method = @original_method
+          stubbee.__metaclass__.send(:define_method, method) do |*args, &block|
+            original_method.call(*args, &block)
+          end
+        else
+          stubbee.__metaclass__.send(:define_method, method, @original_method)
+        end
         stubbee.__metaclass__.send(@original_visibility, method)
       end
     end
