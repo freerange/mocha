4b291b452b76bada147d8a349b9403d915c019d7
James Mead <james@floehopper.org>
2012-08-06 12:03:37 +0100

    Simplify restoration of original method. Fixes #95.
    @tandem-softworks correctly pointed out that the code was overly
    complicated and introduced an unjustificable stack overhead. This is
    also the way `AnyInstanceMethod#restore_original_method` works, so this
    change makes the code more consistent.

--- lib/mocha/class_method.rb
+++ lib/mocha/class_method.rb
@@ -66,10 +66,7 @@ module Mocha
 
     def restore_original_method
       if @original_method && @original_method.owner == stubbee.__metaclass__
-        original_method = @original_method
-        stubbee.__metaclass__.send(:define_method, method) do |*args, &block|
-          original_method.call(*args, &block)
-        end
+        stubbee.__metaclass__.send(:define_method, method, @original_method)
         stubbee.__metaclass__.send(@original_visibility, method)
       end
     end
