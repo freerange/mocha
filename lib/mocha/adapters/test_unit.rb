7869f3897c08d824df670225e170439a3ae484d8
James Mead <james@floehopper.org>
2012-08-24 15:36:30 +0100

    Tweaks to new Test::Unit adapter.
    * It's reassuring to see that the changes I independently arrived at
      in e9d05cc2acf3a0bf5fbc78737d7dfa684e12807e are almost identical to
      those @kou made in #89.
    * This commit brings the code more into line with what @kou recommended,
      although I haven't extracted all the private methods that he did -
      they seem like overkill to me.
    * I'm still curious to know whether #problem_occurred and #add_failure
      are part of the Test::Unit public API...?

--- lib/mocha/adapters/test_unit.rb
+++ lib/mocha/adapters/test_unit.rb
@@ -17,24 +17,26 @@ module Mocha
 
       include Mocha::API
 
-      def handle_mocha_expectation_error(e)
-        return false unless e.is_a?(Mocha::ExpectationError)
-        problem_occurred
-        add_failure(e.message, e.backtrace)
-        true
-      end
-
       def self.included(mod)
         mod.setup :mocha_setup
 
         mod.exception_handler(:handle_mocha_expectation_error)
 
-        mod.cleanup do
+        mod.cleanup :after => :append do
           assertion_counter = AssertionCounter.new(self)
           mocha_verify(assertion_counter)
         end
 
         mod.teardown :mocha_teardown, :after => :append
+      end
+
+      private
+
+      def handle_mocha_expectation_error(e)
+        return false unless e.is_a?(Mocha::ExpectationError)
+        problem_occurred
+        add_failure(e.message, e.backtrace)
+        true
       end
     end
   end
