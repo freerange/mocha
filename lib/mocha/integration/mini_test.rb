d043a55615db8e58cdc673e953890c8b90fa822d
James Mead <james@floehopper.org>
2012-08-24 10:47:59 +0100

    Refactor MiniTest monkey-patching integration.
    Hopefully this makes it a bit easier to see what's going on and I hope
    it will allow me to further simplify things.

--- lib/mocha/integration/mini_test.rb
+++ lib/mocha/integration/mini_test.rb
@@ -13,49 +13,49 @@ require 'mocha/integration/mini_test/version_2112_to_320'
 require 'mocha/integration/mini_test/version_330'
 
 if !MiniTest::Unit::TestCase.ancestors.include?(Mocha::API)
+  MiniTest::Unit::TestCase.send(:include, Mocha::API)
+end
 
-  module MiniTest
-    class Unit
-      class TestCase
-
-        include Mocha::API
-
-        alias_method :run_before_mocha, :run
-        remove_method :run
-
-        mini_test_version = begin
-          Gem::Version.new(MiniTest::Unit::VERSION)
-        rescue LoadError
-          Gem::Version.new('0.0.0')
-        end
-
-        $stderr.puts "Detected MiniTest version: #{mini_test_version}" if $mocha_options['debug']
-
-        if Gem::Requirement.new('>= 1.3.0', '<= 1.3.1').satisfied_by?(mini_test_version)
-          include Mocha::Integration::MiniTest::Version13
-        elsif Gem::Requirement.new('1.4.0').satisfied_by?(mini_test_version)
-          include Mocha::Integration::MiniTest::Version140
-        elsif Gem::Requirement.new('1.4.1').satisfied_by?(mini_test_version)
-          include Mocha::Integration::MiniTest::Version141
-        elsif Gem::Requirement.new('>= 1.4.2', '<= 1.7.2').satisfied_by?(mini_test_version)
-          include Mocha::Integration::MiniTest::Version142To172
-        elsif Gem::Requirement.new('2.0.0').satisfied_by?(mini_test_version)
-          include Mocha::Integration::MiniTest::Version200
-        elsif Gem::Requirement.new('>= 2.0.1', '<= 2.2.2').satisfied_by?(mini_test_version)
-          include Mocha::Integration::MiniTest::Version201To222
-        elsif Gem::Requirement.new('>= 2.3.0', '<= 2.10.1').satisfied_by?(mini_test_version)
-          include Mocha::Integration::MiniTest::Version230To2101
-        elsif Gem::Requirement.new('>= 2.11.0', '<= 2.11.1').satisfied_by?(mini_test_version)
-          include Mocha::Integration::MiniTest::Version2110To2111
-        elsif Gem::Requirement.new('>= 2.11.2', '<= 3.2.0').satisfied_by?(mini_test_version)
-          include Mocha::Integration::MiniTest::Version2112To320
-        elsif Gem::Requirement.new('3.3.0').satisfied_by?(mini_test_version)
-          include Mocha::Integration::MiniTest::Version330
-        else
-          $stderr.puts "*** No Mocha monkey-patch for MiniTest version ***" if $mocha_options['debug']
-        end
-
-      end
-    end
+if !MiniTest::Unit::TestCase.method_defined?(:run_before_mocha)
+  class MiniTest::Unit::TestCase
+    alias_method :run_before_mocha, :run
+    remove_method :run
   end
+end
+
+mini_test_version = begin
+  Gem::Version.new(MiniTest::Unit::VERSION)
+rescue LoadError
+  Gem::Version.new('0.0.0')
+end
+
+$stderr.puts "Detected MiniTest version: #{mini_test_version}" if $mocha_options['debug']
+
+minitest_integration_module = if Gem::Requirement.new('>= 1.3.0', '<= 1.3.1').satisfied_by?(mini_test_version)
+  Mocha::Integration::MiniTest::Version13
+elsif Gem::Requirement.new('1.4.0').satisfied_by?(mini_test_version)
+  Mocha::Integration::MiniTest::Version140
+elsif Gem::Requirement.new('1.4.1').satisfied_by?(mini_test_version)
+  Mocha::Integration::MiniTest::Version141
+elsif Gem::Requirement.new('>= 1.4.2', '<= 1.7.2').satisfied_by?(mini_test_version)
+  Mocha::Integration::MiniTest::Version142To172
+elsif Gem::Requirement.new('2.0.0').satisfied_by?(mini_test_version)
+  Mocha::Integration::MiniTest::Version200
+elsif Gem::Requirement.new('>= 2.0.1', '<= 2.2.2').satisfied_by?(mini_test_version)
+  Mocha::Integration::MiniTest::Version201To222
+elsif Gem::Requirement.new('>= 2.3.0', '<= 2.10.1').satisfied_by?(mini_test_version)
+  Mocha::Integration::MiniTest::Version230To2101
+elsif Gem::Requirement.new('>= 2.11.0', '<= 2.11.1').satisfied_by?(mini_test_version)
+  Mocha::Integration::MiniTest::Version2110To2111
+elsif Gem::Requirement.new('>= 2.11.2', '<= 3.2.0').satisfied_by?(mini_test_version)
+  Mocha::Integration::MiniTest::Version2112To320
+elsif Gem::Requirement.new('3.3.0').satisfied_by?(mini_test_version)
+  Mocha::Integration::MiniTest::Version330
+else
+  $stderr.puts "*** No Mocha monkey-patch for MiniTest version ***" if $mocha_options['debug']
+  nil
+end
+
+if minitest_integration_module && !MiniTest::Unit::TestCase.ancestors.include?(minitest_integration_module)
+  MiniTest::Unit::TestCase.send(:include, minitest_integration_module)
 end
