9ef5615f790552d2e079a0cea061754120358445
James Mead <james@floehopper.org>
2012-08-27 14:03:27 +0100

    Simplified integration code.

--- lib/mocha/integration.rb
+++ lib/mocha/integration.rb
@@ -2,38 +2,22 @@ require 'mocha/deprecation'
 
 module Mocha
   module Integration
-    class << self
-      def monkey_patches
-        patches = []
-        if test_unit_testcase_defined? && !test_unit_testcase_inherits_from_miniunit_testcase?
-          patches << 'mocha/integration/test_unit'
-        end
-        if mini_test_testcase_defined?
-          patches << 'mocha/integration/mini_test'
-        end
-        patches
-      end
-
-      def test_unit_testcase_defined?
-        defined?(Test::Unit::TestCase)
-      end
-
-      def mini_test_testcase_defined?
-        defined?(MiniTest::Unit::TestCase)
-      end
-
-      def test_unit_testcase_inherits_from_miniunit_testcase?
-        test_unit_testcase_defined? && mini_test_testcase_defined? && (Test::Unit::TestCase < MiniTest::Unit::TestCase)
-      end
-    end
   end
 end
 
-unless Mocha::Integration.monkey_patches.any?
-  Mocha::Deprecation.warning("Test::Unit or MiniTest must be loaded *before* Mocha.")
-  Mocha::Deprecation.warning("If you're integrating with another test library, you should probably require 'mocha_standalone' instead of 'mocha'")
+integration_files = []
+if defined?(Test::Unit::TestCase) && !(defined?(MiniTest::Unit::TestCase) && (Test::Unit::TestCase < MiniTest::Unit::TestCase))
+  integration_files << 'mocha/integration/test_unit'
+end
+if defined?(MiniTest::Unit::TestCase)
+  integration_files << 'mocha/integration/mini_test'
 end
 
-Mocha::Integration.monkey_patches.each do |patch|
-  require patch
+if integration_files.any?
+  integration_files.each do |integration_file|
+    require integration_file
+  end
+else
+  Mocha::Deprecation.warning("Test::Unit or MiniTest must be loaded *before* Mocha.")
+  Mocha::Deprecation.warning("If you're integrating with another test library, you should probably require 'mocha_standalone' instead of 'mocha'")
 end
