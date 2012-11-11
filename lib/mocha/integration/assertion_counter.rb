b8738e788cec8c8ab5f7db8fff47809cce9b806d
James Mead <james@floehopper.org>
2012-11-11 17:23:09 +0000

    Fix whitespace.

--- lib/mocha/integration/assertion_counter.rb
+++ lib/mocha/integration/assertion_counter.rb
@@ -2,7 +2,7 @@ module Mocha
   module Integration
     class AssertionCounter
       def initialize(test_case)
-        @test_case= test_case
+        @test_case = test_case
       end
 
       def increment
