fffaf54f6030b2cd95a0558c54c5f081ce390f4c
Peter Boling <peter.boling@gmail.com>
2013-01-06 13:53:52 -0500

    Adds caller backtrace to deprecation warnings, so the offenders can be discovered more easily

--- lib/mocha.rb
+++ lib/mocha.rb
@@ -2,7 +2,7 @@ require 'mocha/version'
 require 'mocha/integration'
 require 'mocha/deprecation'
 
-Mocha::Deprecation.warning("Change `require 'mocha'` to `require 'mocha/setup'`.")
+Mocha::Deprecation.warning("Change `require 'mocha'` to `require 'mocha/setup'`. Called from:\n#{caller.join("\n")}")
 
 require 'mocha/setup'
 
