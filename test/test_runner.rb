cfd0262c21b29bbdc8fb240cfee2ab80dbc8fdf2
James Mead <james@floehopper.org>
2013-12-27 17:42:24 +0000

    Fix indentation in `test/test_runner.rb`.

--- test/test_runner.rb
+++ test/test_runner.rb
@@ -20,22 +20,22 @@ module TestRunner
     tests = methods.keys.select { |m| m.to_s[/^test/] }.map { |m| test_class.new(m) }
 
     if Mocha::Detection::MiniTest.testcase && (ENV['MOCHA_RUN_INTEGRATION_TESTS'] != 'test-unit')
-    minitest_version = Gem::Version.new(Mocha::Detection::MiniTest.version)
-    if Gem::Requirement.new('>= 5.0.0').satisfied_by?(minitest_version)
-      require File.expand_path('../minitest_result', __FILE__)
-      tests.each do |test|
-        test.run
+      minitest_version = Gem::Version.new(Mocha::Detection::MiniTest.version)
+      if Gem::Requirement.new('>= 5.0.0').satisfied_by?(minitest_version)
+        require File.expand_path('../minitest_result', __FILE__)
+        tests.each do |test|
+          test.run
+        end
+        Minitest::Runnable.runnables.delete(test_class)
+        test_result = MinitestResult.new(tests)
+      elsif Gem::Requirement.new('> 0.0.0', '< 5.0.0').satisfied_by?(minitest_version)
+        require File.expand_path('../mini_test_result', __FILE__)
+        runner = MiniTest::Unit.new
+        tests.each do |test|
+          test.run(runner)
+        end
+        test_result = MiniTestResult.new(runner, tests)
       end
-      Minitest::Runnable.runnables.delete(test_class)
-      test_result = MinitestResult.new(tests)
-    elsif Gem::Requirement.new('> 0.0.0', '< 5.0.0').satisfied_by?(minitest_version)
-      require File.expand_path('../mini_test_result', __FILE__)
-      runner = MiniTest::Unit.new
-      tests.each do |test|
-        test.run(runner)
-      end
-      test_result = MiniTestResult.new(runner, tests)
-    end
     else
       require File.expand_path('../test_unit_result', __FILE__)
       test_result = TestUnitResult.build_test_result
