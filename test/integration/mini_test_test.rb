6a6c8ddc2336dad3df8e839c0cfc71c0208a6224
James Mead <james@floehopper.org>
2013-12-28 18:30:07 +0000

    Provide shortcuts for integrating with specific test library.
    Now you can use:
    
        require 'mocha/test_unit'
    
    OR
    
        require 'mocha/mini_test'
    
    Instead of:
    
        require "mocha/integration/test_unit"
    
        Mocha::Integration::TestUnit.activate
    
    OR
    
        require "mocha/integration/mini_test"
    
        Mocha::Integration::MiniTest.activate
    
    Note: both of these assume Mocha itself has been required either explicitly:
    
        require 'mocha'
    
    OR
    
        # Auto-required via bundler.

--- test/integration/mini_test_test.rb
+++ test/integration/mini_test_test.rb
@@ -1,9 +1,7 @@
 require File.expand_path('../../test_helper', __FILE__)
 
-require "mocha/integration/mini_test"
+require "mocha/mini_test"
 require "integration/shared_tests"
-
-Mocha::Integration::MiniTest.activate
 
 class MiniTestTest < Mocha::TestCase
   include SharedTests
