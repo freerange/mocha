1aae332d296fabbefc2c2471ec072d85193221cf
James Mead <james@floehopper.org>
2013-12-28 17:57:58 +0000

    Remove unnecessary code in integration tests.
    `test/test_helper.rb` already loads MiniTest or Test::Unit, so no need to do
    it again here.
    
    Also the "outer" test case just needs to be of a type that can run Mocha
    acceptance tests i.e. it's the "inner" test case that needs to be either
    a `MiniTest::Unit::TestCase`, `Minitest::Test` or `Test::Unit::TestCase`
    as appropriate. The simplest thing to do is to make use of `Mocha::TestCase`
    which is already begin defined in `test/test_helper.rb`.

--- test/integration/mini_test_test.rb
+++ test/integration/mini_test_test.rb
@@ -1,24 +1,10 @@
 require File.expand_path('../../test_helper', __FILE__)
 
-begin
-  require "minitest"
-rescue LoadError
-end
-begin
-  require "minitest/unit"
-rescue LoadError
-end
-
 require "mocha/integration/mini_test"
-require "mocha/detection/mini_test"
 require "integration/shared_tests"
 
 Mocha::Integration::MiniTest.activate
 
-if testcase = Mocha::Detection::MiniTest.testcase
-  class MiniTestTest < testcase
-    include SharedTests
-  end
-else
-  puts "MiniTest not available - no point in running tests."
+class MiniTestTest < Mocha::TestCase
+  include SharedTests
 end
--- test/integration/test_unit_test.rb
+++ test/integration/test_unit_test.rb
@@ -1,11 +1,10 @@
 require File.expand_path('../../test_helper', __FILE__)
 
-require "test/unit"
 require "mocha/integration/test_unit"
 require "integration/shared_tests"
 
 Mocha::Integration::TestUnit.activate
 
-class TestUnitTest < Test::Unit::TestCase
+class TestUnitTest < Mocha::TestCase
   include SharedTests
 end
