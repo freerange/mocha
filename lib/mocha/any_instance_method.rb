5ab1e6a26f410f4f97a286c4bbff15b958f6c5dd
Albert Llop <mrsimo@gmail.com>
2014-05-12 09:43:25 +0200

    No need to set the visibility on stubbed method for any_instance

--- lib/mocha/any_instance_method.rb
+++ lib/mocha/any_instance_method.rb
@@ -39,9 +39,6 @@ module Mocha
           self.class.any_instance.mocha.method_missing(:#{method}, *args, &block)
         end
       CODE
-      if @original_visibility
-        Module.instance_method(@original_visibility).bind(definition_target).call(method)
-      end
     end
 
     def remove_new_method
