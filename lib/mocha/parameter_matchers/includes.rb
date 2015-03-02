f166fa9e64fa04eb649e108615e7a34320247310
James Adam <james@lazyatom.com>
2015-03-02 17:44:12 -0600

    Allow `includes` matcher to take matcher arguments.
    We should be able to created a matcher of the form:
    
        object.expects(:foo).with(includes(has_entry(:key, 'value')))
    
    and successfully match this against a method call like:
    
        object.foo([:a, :b, {:key => value}, :c])

--- lib/mocha/parameter_matchers/includes.rb
+++ lib/mocha/parameter_matchers/includes.rb
@@ -22,6 +22,16 @@ module Mocha
     # @example Actual parameter does not include all items.
     #   object.method_1(['foo', 'baz'])
     #   # error raised, because ['foo', 'baz'] does not include 'bar'.
+    #
+    # @example Actual parameter includes item which matches nested matcher.
+    #   object = mock()
+    #   object.expects(:method_1).with(includes(has_key(:key)))
+    #   object.method_1(['foo', 'bar', {:key => 'baz'}])
+    #   # no error raised
+    #
+    # @example Actual parameter does not include item matching nested matcher.
+    #   object.method_1(['foo', 'bar', {:other_key => 'baz'}])
+    #   # error raised, because no element matches `has_key(:key)` matcher
     def includes(*items)
       Includes.new(*items)
     end
@@ -38,9 +48,8 @@ module Mocha
       def matches?(available_parameters)
         parameter = available_parameters.shift
         return false unless parameter.respond_to?(:include?)
-
         if @items.size == 1
-          return parameter.include?(@items.first)
+          return parameter.any? { |p| @items.first.to_matcher.matches?([p]) }
         else
           includes_matchers = @items.map { |item| Includes.new(item) }
           AllOf.new(*includes_matchers).matches?([parameter])
