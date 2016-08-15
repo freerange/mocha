6442acf2ae85c34af15b5b9271baf0379e549966
James Mead <james@floehopper.org>
2016-08-15 15:05:58 +0100

    Split long line into multiple lines

--- lib/mocha/class_method.rb
+++ lib/mocha/class_method.rb
@@ -103,7 +103,9 @@ module Mocha
     def method_exists?(method)
       symbol = method.to_sym
       __metaclass__ = stubbee.__metaclass__
-      __metaclass__.public_method_defined?(symbol) || __metaclass__.protected_method_defined?(symbol) || __metaclass__.private_method_defined?(symbol)
+      __metaclass__.public_method_defined?(symbol) ||
+        __metaclass__.protected_method_defined?(symbol) ||
+        __metaclass__.private_method_defined?(symbol)
     end
 
     private
