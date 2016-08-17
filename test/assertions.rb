a4ba95bb04995721e3f46423a86ffbf0e4a9f079
James Mead <james@floehopper.org>
2016-08-17 15:42:00 +0100

    Check all accessible methods when asserting visibility
    We're planning to use the prepended module stubbing mechanism more widely for
    Ruby v2+. This means that the assertion needs to consider methods which are not
    defined on `object`. We're happy to do this since all the places where this
    assertion is used are looking for a method with an unusual name, i.e. there's no
    real possibility that the check would accidentally find the method on another of
    the ancestors of `object`.

--- test/assertions.rb
+++ test/assertions.rb
@@ -3,6 +3,6 @@ require 'mocha/ruby_version'
 module Assertions
   def assert_method_visiblity(object, method_name, visiblity)
     method_key = Mocha::PRE_RUBY_V19 ? method_name.to_s : method_name.to_sym
-    assert object.send("#{visiblity}_methods", false).include?(method_key), "#{method_name} is not #{visiblity}"
+    assert object.send("#{visiblity}_methods").include?(method_key), "#{method_name} is not #{visiblity}"
   end
 end
