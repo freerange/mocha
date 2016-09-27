6a29f9e83b0b621a7997562dbb06f5f24015a193
Chris Roos <chris@seagul.co.uk>
2016-09-27 18:42:21 +0100

    Extract ClassMethod#default_definition_target
    And override the implementation in
    `AnyInstanceMethod#default_definition_target`.
    
    I want to make the implementations of `ClassMethod#definition_target`
    and `AnyInstanceMethod#definition_target` identical so that I can
    eventually remove `AnyInstanceMethod#definition_target`.

--- lib/mocha/class_method.rb
+++ lib/mocha/class_method.rb
@@ -128,7 +128,11 @@ module Mocha
     end
 
     def definition_target
-      @definition_target ||= stubbee.__metaclass__
+      @definition_target ||= default_definition_target
+    end
+
+    def default_definition_target
+      stubbee.__metaclass__
     end
   end
 end
