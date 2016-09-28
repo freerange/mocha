477a23f59193dfc0eaec4f2ce5d1fd849d567ef7
Chris Roos <chris@seagul.co.uk>
2016-09-28 11:37:33 +0100

    Remove AnyInstanceMethod#prepend_module
    Now that the implementation is the same as its parent's.

--- lib/mocha/any_instance_method.rb
+++ lib/mocha/any_instance_method.rb
@@ -42,11 +42,6 @@ module Mocha
       default_stub_method_owner.send(:remove_method, method_name)
     end
 
-    def prepend_module
-      @stub_method_owner = PrependedModule.new
-      default_stub_method_owner.__send__ :prepend, @stub_method_owner
-    end
-
     def stub_method_definition
       filename, line_number_of_method_implementation = __FILE__, __LINE__ + 2
       method_implementation = <<-CODE
