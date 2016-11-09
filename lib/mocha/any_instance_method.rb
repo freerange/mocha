5036ef7bf099085f742e46d6c693e765949e4d9e
Chris Roos <chris@seagul.co.uk>
2016-11-09 16:17:02 +0000

    Remove AnyInstanceMethod#original_method_defined_on_stubbee?
    Now that the implementation is the same as its parent's (`ClassMethod`).

--- lib/mocha/any_instance_method.rb
+++ lib/mocha/any_instance_method.rb
@@ -34,10 +34,6 @@ module Mocha
       @original_method = default_stub_method_owner.instance_method(method_name)
     end
 
-    def original_method_defined_on_stubbee?
-      original_method && original_method.owner == default_stub_method_owner
-    end
-
     def stub_method_definition
       filename, line_number_of_method_implementation = __FILE__, __LINE__ + 2
       method_implementation = <<-CODE
