9215d7b3e3b5ed3041ca609b1526e08aef6b9c96
Akira Matsuda <ronnie@dio.jp>
2017-09-24 22:08:54 +0900

    :warning: assigned but unused variable
    include_public_methods, include_superclass_methods, and include_superclass_methods

--- lib/mocha/object_methods.rb
+++ lib/mocha/object_methods.rb
@@ -162,11 +162,11 @@ module Mocha
     # @private
     def method_exists?(method, include_public_methods = true)
       if include_public_methods
-        return true if public_methods(include_superclass_methods = true).include?(method)
+        return true if public_methods(_include_superclass_methods = true).include?(method)
         return true if respond_to?(method.to_sym)
       end
-      return true if protected_methods(include_superclass_methods = true).include?(method)
-      return true if private_methods(include_superclass_methods = true).include?(method)
+      return true if protected_methods(_include_superclass_methods = true).include?(method)
+      return true if private_methods(_include_superclass_methods = true).include?(method)
       return false
     end
 
