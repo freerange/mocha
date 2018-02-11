55d66ed4f5e6cad95e1605341ec3c608be2c6833
James Mead <james@floehopper.org>
2018-02-11 15:59:12 +0000

    Rename mocha/mini_test.rb to mocha/minitest.rb
    * Deprecate requiring 'mocha/mini_test'
    * Update documentation in README

--- test/integration/mini_test_test.rb
+++ test/integration/mini_test_test.rb
@@ -1,6 +1,6 @@
 require File.expand_path('../../test_helper', __FILE__)
 
-require "mocha/mini_test"
+require "mocha/minitest"
 require "integration/shared_tests"
 
 class MiniTestTest < Mocha::TestCase
