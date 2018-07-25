3291e3fcc2a2dedbeac0b8236848fd37c13532eb
James Mead <3169+floehopper@users.noreply.github.com>
2018-07-25 20:39:32 +0100

    Fix Style/IfInsideElse violations manually

--- lib/mocha/cardinality.rb
+++ lib/mocha/cardinality.rb
@@ -54,18 +54,16 @@ module Mocha
     def mocha_inspect
       if allowed_any_number_of_times?
         "allowed any number of times"
+      elsif required == 0 && maximum == 0
+        "expected never"
+      elsif required == maximum
+        "expected exactly #{times(required)}"
+      elsif infinite?(maximum)
+        "expected at least #{times(required)}"
+      elsif required == 0
+        "expected at most #{times(maximum)}"
       else
-        if required == 0 && maximum == 0
-          "expected never"
-        elsif required == maximum
-          "expected exactly #{times(required)}"
-        elsif infinite?(maximum)
-          "expected at least #{times(required)}"
-        elsif required == 0
-          "expected at most #{times(maximum)}"
-        else
-          "expected between #{required} and #{times(maximum)}"
-        end
+        "expected between #{required} and #{times(maximum)}"
       end
     end
 
--- lib/mocha/mock.rb
+++ lib/mocha/mock.rb
@@ -297,18 +297,16 @@ module Mocha
       end
       if (matching_expectation_allowing_invocation = all_expectations.match_allowing_invocation(symbol, *arguments))
         matching_expectation_allowing_invocation.invoke(&block)
-      else
-        if (matching_expectation = all_expectations.match(symbol, *arguments)) || (!matching_expectation && !@everything_stubbed)
-          if @unexpected_invocation.nil?
-            @unexpected_invocation = UnexpectedInvocation.new(self, symbol, *arguments)
-            matching_expectation.invoke(&block) if matching_expectation
-            message = @unexpected_invocation.full_description
-            message << @mockery.mocha_inspect
-          else
-            message = @unexpected_invocation.short_description
-          end
-          raise ExpectationErrorFactory.build(message, caller)
+      elsif (matching_expectation = all_expectations.match(symbol, *arguments)) || (!matching_expectation && !@everything_stubbed)
+        if @unexpected_invocation.nil?
+          @unexpected_invocation = UnexpectedInvocation.new(self, symbol, *arguments)
+          matching_expectation.invoke(&block) if matching_expectation
+          message = @unexpected_invocation.full_description
+          message << @mockery.mocha_inspect
+        else
+          message = @unexpected_invocation.short_description
         end
+        raise ExpectationErrorFactory.build(message, caller)
       end
     end
 
