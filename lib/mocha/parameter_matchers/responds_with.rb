9df7178b54749f4d0276eae0377fa27c39217dec
James Mead <3169+floehopper@users.noreply.github.com>
2018-07-25 14:49:42 +0100

    Fix Layout/SpaceInsideParens violations using auto-correct

--- lib/mocha/parameter_matchers/responds_with.rb
+++ lib/mocha/parameter_matchers/responds_with.rb
@@ -36,7 +36,7 @@ module Mocha
       # @private
       def matches?(available_parameters)
         parameter = available_parameters.shift
-        @result.to_matcher.matches?( [parameter.__send__(@message)] )
+        @result.to_matcher.matches?([parameter.__send__(@message)])
       end
 
       # @private
