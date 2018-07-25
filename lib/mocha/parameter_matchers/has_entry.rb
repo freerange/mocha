a5208035c16d03a6f09a24c6e6454e88909c302c
James Mead <3169+floehopper@users.noreply.github.com>
2018-07-25 23:05:54 +0100

    Fix Style/RaiseArgs violations using auto-correct

--- lib/mocha/parameter_matchers/has_entry.rb
+++ lib/mocha/parameter_matchers/has_entry.rb
@@ -47,19 +47,19 @@ module Mocha
         when Hash
           case options[0].length
           when 0
-            raise ArgumentError.new("Argument has no entries.")
+            raise ArgumentError, "Argument has no entries."
           when 1
             key, value = options[0].first
           else
-            raise ArgumentError.new("Argument has multiple entries. Use Mocha::ParameterMatchers#has_entries instead.")
+            raise ArgumentError, "Argument has multiple entries. Use Mocha::ParameterMatchers#has_entries instead."
           end
         else
-          raise ArgumentError.new("Argument is not a Hash.")
+          raise ArgumentError, "Argument is not a Hash."
         end
       when 2
         key, value = options
       else
-        raise ArgumentError.new("Too many arguments; use either a single argument (must be a Hash) or two arguments (a key and a value).")
+        raise ArgumentError, "Too many arguments; use either a single argument (must be a Hash) or two arguments (a key and a value)."
       end
       HasEntry.new(key, value)
     end
