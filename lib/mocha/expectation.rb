9994f565c262ef3bc936cfaaed2eb6c7fc21a430
James Mead <3169+floehopper@users.noreply.github.com>
2018-07-25 14:31:58 +0100

    Fix Layout/EndAlignment violation
    Note that auto-correct didn't work, so I fixed this manually.

--- lib/mocha/expectation.rb
+++ lib/mocha/expectation.rb
@@ -593,7 +593,7 @@ module Mocha
                  when 1 then "invoked once"
                  when 2 then "invoked twice"
                  else "invoked #{@invocation_count} times"
-      end
+                 end
       message << ": "
       message << method_signature
       message << "; #{@ordering_constraints.map { |oc| oc.mocha_inspect }.join("; ")}" unless @ordering_constraints.empty?
