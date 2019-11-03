ab619c863cc3928d5a79fd7bae333ab78752605a
James Mead <james@floehopper.org>
2019-11-03 18:55:02 +0000

    Distinguish module defining to_matcher method
    Previously this Mocha::ObjectMethods module was re-opening the module
    defined in `lib/mocha/object_methods.rb`. I think this was potentially
    confusing and so I've moved the to_matcher method definition to a new
    Mocha::ParameterMatchers::InstanceMethods module to make things clearer.

--- test/unit/parameter_matchers/has_key_test.rb
+++ test/unit/parameter_matchers/has_key_test.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../../../test_helper', __FILE__)
 
 require 'mocha/parameter_matchers/has_key'
-require 'mocha/parameter_matchers/object'
+require 'mocha/parameter_matchers/instance_methods'
 require 'mocha/inspect'
 
 class HasKeyTest < Mocha::TestCase
