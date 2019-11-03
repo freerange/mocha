2aa0beef2447e14a7574c10647185ff209700a46
James Mead <james@floehopper.org>
2019-11-03 16:39:01 +0000

    Move MethodDefiner module out of Mocha namespace
    Since this module is not used within the library itself, there's no need
    to include it within the Mocha namespace.

--- test/method_definer.rb
+++ test/method_definer.rb
@@ -1,22 +1,20 @@
 require 'mocha/singleton_class'
 
-module Mocha
-  module MethodDefiner
-    def define_instance_method(method_symbol, &block)
-      singleton_class.send(:define_method, method_symbol, block)
-    end
+module MethodDefiner
+  def define_instance_method(method_symbol, &block)
+    singleton_class.send(:define_method, method_symbol, block)
+  end
 
-    def replace_instance_method(method_symbol, &block)
-      raise "Cannot replace #{method_symbol} as #{self} does not respond to it." unless respond_to?(method_symbol)
-      define_instance_method(method_symbol, &block)
-    end
+  def replace_instance_method(method_symbol, &block)
+    raise "Cannot replace #{method_symbol} as #{self} does not respond to it." unless respond_to?(method_symbol)
+    define_instance_method(method_symbol, &block)
+  end
 
-    def define_instance_accessor(*symbols)
-      symbols.each { |symbol| singleton_class.send(:attr_accessor, symbol) }
-    end
+  def define_instance_accessor(*symbols)
+    symbols.each { |symbol| singleton_class.send(:attr_accessor, symbol) }
   end
 end
 
 class Object
-  include Mocha::MethodDefiner
+  include MethodDefiner
 end
