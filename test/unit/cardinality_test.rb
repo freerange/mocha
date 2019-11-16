2a1240e6ad7bfacd36060c2043f0c2e3f49d2e68
James Mead <james@floehopper.org>
2019-11-16 18:05:57 +0000

    Split Cardinality test method up

--- test/unit/cardinality_test.rb
+++ test/unit/cardinality_test.rb
@@ -33,22 +33,27 @@ class CardinalityTest < Mocha::TestCase
     assert cardinality.satisfied?
   end
 
-  def test_should_describe_cardinality
+  def test_should_describe_cardinality_defined_using_at_least
     assert_equal 'allowed any number of times', Cardinality.at_least(0).anticipated_times
-
-    assert_equal 'expected at most once', Cardinality.at_most(1).anticipated_times
-    assert_equal 'expected at most twice', Cardinality.at_most(2).anticipated_times
-    assert_equal 'expected at most 3 times', Cardinality.at_most(3).anticipated_times
-
     assert_equal 'expected at least once', Cardinality.at_least(1).anticipated_times
     assert_equal 'expected at least twice', Cardinality.at_least(2).anticipated_times
     assert_equal 'expected at least 3 times', Cardinality.at_least(3).anticipated_times
+  end
 
+  def test_should_describe_cardinality_defined_using_at_most
+    assert_equal 'expected at most once', Cardinality.at_most(1).anticipated_times
+    assert_equal 'expected at most twice', Cardinality.at_most(2).anticipated_times
+    assert_equal 'expected at most 3 times', Cardinality.at_most(3).anticipated_times
+  end
+
+  def test_should_describe_cardinality_defined_using_exactly
     assert_equal 'expected never', Cardinality.exactly(0).anticipated_times
     assert_equal 'expected exactly once', Cardinality.exactly(1).anticipated_times
     assert_equal 'expected exactly twice', Cardinality.exactly(2).anticipated_times
     assert_equal 'expected exactly 3 times', Cardinality.exactly(3).anticipated_times
+  end
 
+  def test_should_describe_cardinality_defined_using_times_with_range
     assert_equal 'expected between 2 and 4 times', Cardinality.times(2..4).anticipated_times
     assert_equal 'expected between 1 and 3 times', Cardinality.times(1..3).anticipated_times
   end
