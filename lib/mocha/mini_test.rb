c44895cd9c97b2995d56d3ba8e28453fac231f40
James Mead <james@floehopper.org>
2019-11-16 22:42:41 +0000

    Remove deprecated mocha/mini_test.rb
    This file was renamed to mocha/minitest.rb in v1.4.0 in this commit [1]
    and can now safely be removed.
    
    [1]: https://github.com/freerange/mocha/commit/55d66ed4f5e6cad95e1605341ec3c608be2c6833

--- lib/mocha/mini_test.rb
+++
@@ -1,5 +0,0 @@
-require 'mocha/deprecation'
-
-Mocha::Deprecation.warning("`require 'mocha/mini_test'` has been deprecated. Please use `require 'mocha/minitest' instead.")
-
-require 'mocha/minitest'
