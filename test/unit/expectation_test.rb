077e8578f7f617ab19fd6d4931118a9381e8f7e0
Nitish Rathi <nitishrathi@gmail.com>
2019-11-17 08:19:41 +0000

    Extract ExpectationTest#invoke
    In preparation for passing Invocation instance to Expectation#invoke.

--- test/unit/expectation_test.rb
+++ test/unit/expectation_test.rb
@@ -12,6 +12,10 @@ class ExpectationTest < Mocha::TestCase
     Expectation.new(nil, :expected_method)
   end
 
+  def invoke(expectation, &block)
+    expectation.invoke(&block)
+  end
+
   def test_should_match_calls_to_same_method_with_any_parameters
     assert new_expectation.match?(Invocation.new(:irrelevant, :expected_method, 1, 2, 3))
   end
@@ -62,7 +66,7 @@ class ExpectationTest < Mocha::TestCase
   def test_should_allow_invocations_until_expected_invocation_count_is_one_and_actual_invocation_count_would_be_two
     expectation = new_expectation.times(1)
     assert expectation.invocations_allowed?
-    expectation.invoke
+    invoke(expectation)
     assert !expectation.invocations_allowed?
   end
 
@@ -70,7 +74,7 @@ class ExpectationTest < Mocha::TestCase
     expectation = new_expectation.times(2)
     2.times do
       assert expectation.invocations_allowed?
-      expectation.invoke
+      invoke(expectation)
     end
     assert !expectation.invocations_allowed?
   end
@@ -79,7 +83,7 @@ class ExpectationTest < Mocha::TestCase
     expectation = new_expectation.times(2..3)
     3.times do
       assert expectation.invocations_allowed?
-      expectation.invoke
+      invoke(expectation)
     end
     assert !expectation.invocations_allowed?
   end
@@ -98,133 +102,133 @@ class ExpectationTest < Mocha::TestCase
 
   def test_should_not_yield
     yielded = false
-    new_expectation.invoke { yielded = true }
+    invoke(new_expectation) { yielded = true }
     assert_equal false, yielded
   end
 
   def test_should_yield_no_parameters
     yielded_parameters = nil
-    new_expectation.yields.invoke { |*parameters| yielded_parameters = parameters }
+    invoke(new_expectation.yields) { |*parameters| yielded_parameters = parameters }
     assert_equal [], yielded_parameters
   end
 
   def test_yield_should_fail_when_the_caller_does_not_provide_a_block
-    assert_raises(LocalJumpError) { new_expectation.yields(:foo).invoke }
+    assert_raises(LocalJumpError) { invoke(new_expectation.yields(:foo)) }
   end
 
   def test_should_yield_with_specified_parameters
     yielded_parameters = nil
-    new_expectation.yields(1, 2, 3).invoke { |*parameters| yielded_parameters = parameters }
+    invoke(new_expectation.yields(1, 2, 3)) { |*parameters| yielded_parameters = parameters }
     assert_equal [1, 2, 3], yielded_parameters
   end
 
   def test_should_yield_different_parameters_on_consecutive_invocations
     expectation = new_expectation.yields(1, 2, 3).yields(4, 5)
     yielded_parameters = []
-    expectation.invoke { |*parameters| yielded_parameters << parameters }
-    expectation.invoke { |*parameters| yielded_parameters << parameters }
+    invoke(expectation) { |*parameters| yielded_parameters << parameters }
+    invoke(expectation) { |*parameters| yielded_parameters << parameters }
     assert_equal [[1, 2, 3], [4, 5]], yielded_parameters
   end
 
   def test_should_yield_multiple_times_for_single_invocation
     yielded_parameters = []
-    new_expectation.multiple_yields([1, 2, 3], [4, 5]).invoke { |*parameters| yielded_parameters << parameters }
+    invoke(new_expectation.multiple_yields([1, 2, 3], [4, 5])) { |*parameters| yielded_parameters << parameters }
     assert_equal [[1, 2, 3], [4, 5]], yielded_parameters
   end
 
   def test_should_yield_multiple_times_for_first_invocation_and_once_for_second_invocation
     expectation = new_expectation.multiple_yields([1, 2, 3], [4, 5]).then.yields(6, 7)
     yielded_parameters = []
-    expectation.invoke { |*parameters| yielded_parameters << parameters }
-    expectation.invoke { |*parameters| yielded_parameters << parameters }
+    invoke(expectation) { |*parameters| yielded_parameters << parameters }
+    invoke(expectation) { |*parameters| yielded_parameters << parameters }
     assert_equal [[1, 2, 3], [4, 5], [6, 7]], yielded_parameters
   end
 
   def test_should_return_specified_value
-    assert_equal 99, new_expectation.returns(99).invoke
+    assert_equal 99, invoke(new_expectation.returns(99))
   end
 
   def test_should_return_same_specified_value_multiple_times
     expectation = new_expectation.returns(99)
-    assert_equal 99, expectation.invoke
-    assert_equal 99, expectation.invoke
+    assert_equal 99, invoke(expectation)
+    assert_equal 99, invoke(expectation)
   end
 
   def test_should_return_specified_values_on_consecutive_calls
     expectation = new_expectation.returns(99, 100, 101)
-    assert_equal 99, expectation.invoke
-    assert_equal 100, expectation.invoke
-    assert_equal 101, expectation.invoke
+    assert_equal 99, invoke(expectation)
+    assert_equal 100, invoke(expectation)
+    assert_equal 101, invoke(expectation)
   end
 
   def test_should_return_specified_values_on_consecutive_calls_even_if_values_are_modified
     values = [99, 100, 101]
     expectation = new_expectation.returns(*values)
     values.shift
-    assert_equal 99, expectation.invoke
-    assert_equal 100, expectation.invoke
-    assert_equal 101, expectation.invoke
+    assert_equal 99, invoke(expectation)
+    assert_equal 100, invoke(expectation)
+    assert_equal 101, invoke(expectation)
   end
 
   def test_should_return_nil_by_default
-    assert_nil new_expectation.invoke
+    assert_nil invoke(new_expectation)
   end
 
   def test_should_return_nil_if_no_value_specified
-    assert_nil new_expectation.returns.invoke
+    assert_nil invoke(new_expectation.returns)
   end
 
   def test_should_raise_runtime_exception
-    assert_raises(RuntimeError) { new_expectation.raises.invoke }
+    assert_raises(RuntimeError) { invoke(new_expectation.raises) }
   end
 
   def test_should_raise_custom_exception
     exception = Class.new(Exception)
-    assert_raises(exception) { new_expectation.raises(exception).invoke }
+    assert_raises(exception) { invoke(new_expectation.raises(exception)) }
   end
 
   def test_should_raise_same_instance_of_custom_exception
     exception_klass = Class.new(StandardError)
     expected_exception = exception_klass.new
-    actual_exception = assert_raises(exception_klass) { new_expectation.raises(expected_exception).invoke }
+    actual_exception = assert_raises(exception_klass) { invoke(new_expectation.raises(expected_exception)) }
     assert_same expected_exception, actual_exception
   end
 
   def test_should_use_the_default_exception_message
-    exception = assert_raises(Exception) { new_expectation.raises(Exception).invoke }
+    exception = assert_raises(Exception) { invoke(new_expectation.raises(Exception)) }
     assert_equal Exception.new.message, exception.message
   end
 
   def test_should_raise_custom_exception_with_message
     exception_msg = 'exception message'
-    exception = assert_raises(Exception) { new_expectation.raises(Exception, exception_msg).invoke }
+    exception = assert_raises(Exception) { invoke(new_expectation.raises(Exception, exception_msg)) }
     assert_equal exception_msg, exception.message
   end
 
   def test_should_return_values_then_raise_exception
     expectation = new_expectation.returns(1, 2).then.raises
-    assert_equal 1, expectation.invoke
-    assert_equal 2, expectation.invoke
-    assert_raises(RuntimeError) { expectation.invoke }
+    assert_equal 1, invoke(expectation)
+    assert_equal 2, invoke(expectation)
+    assert_raises(RuntimeError) { invoke(expectation) }
   end
 
   def test_should_raise_exception_then_return_values
     expectation = new_expectation.raises.then.returns(1, 2)
-    assert_raises(RuntimeError) { expectation.invoke }
-    assert_equal 1, expectation.invoke
-    assert_equal 2, expectation.invoke
+    assert_raises(RuntimeError) { invoke(expectation) }
+    assert_equal 1, invoke(expectation)
+    assert_equal 2, invoke(expectation)
   end
 
   def test_should_verify_successfully_if_expected_call_was_made
     expectation = new_expectation
-    expectation.invoke
+    invoke(expectation)
     assert expectation.verified?
   end
 
   def test_should_not_verify_successfully_if_call_expected_once_but_invoked_twice
     expectation = new_expectation.once
-    expectation.invoke
-    expectation.invoke
+    invoke(expectation)
+    invoke(expectation)
     assert !expectation.verified?
   end
 
@@ -234,34 +238,34 @@ class ExpectationTest < Mocha::TestCase
 
   def test_should_verify_successfully_if_call_expected_once_and_invoked_once
     expectation = new_expectation.once
-    expectation.invoke
+    invoke(expectation)
     assert expectation.verified?
   end
 
   def test_should_not_verify_successfully_if_call_expected_twice_and_invoked_three_times
     expectation = new_expectation.twice
-    expectation.invoke
-    expectation.invoke
-    expectation.invoke
+    invoke(expectation)
+    invoke(expectation)
+    invoke(expectation)
     assert !expectation.verified?
   end
 
   def test_should_not_verify_successfully_if_call_expected_twice_but_invoked_once
     expectation = new_expectation.twice
-    expectation.invoke
+    invoke(expectation)
     assert !expectation.verified?
   end
 
   def test_should_verify_successfully_if_call_expected_twice_and_invoked_twice
     expectation = new_expectation.twice
-    expectation.invoke
-    expectation.invoke
+    invoke(expectation)
+    invoke(expectation)
     assert expectation.verified?
   end
 
   def test_should_verify_successfully_if_expected_call_was_made_at_least_once
     expectation = new_expectation.at_least_once
-    3.times { expectation.invoke }
+    3.times { invoke(expectation) }
     assert expectation.verified?
   end
 
@@ -273,26 +277,26 @@ class ExpectationTest < Mocha::TestCase
 
   def test_should_verify_successfully_if_expected_call_was_made_expected_number_of_times
     expectation = new_expectation.times(2)
-    2.times { expectation.invoke }
+    2.times { invoke(expectation) }
     assert expectation.verified?
   end
 
   def test_should_not_verify_successfully_if_expected_call_was_made_too_few_times
     expectation = new_expectation.times(2)
-    1.times { expectation.invoke }
+    1.times { invoke(expectation) }
     assert !expectation.verified?
     assert_match(/expected exactly twice, invoked once/i, expectation.mocha_inspect)
   end
 
   def test_should_not_verify_successfully_if_expected_call_was_made_too_many_times
     expectation = new_expectation.times(2)
-    3.times { expectation.invoke }
+    3.times { invoke(expectation) }
     assert !expectation.verified?
   end
 
   def test_should_increment_assertion_counter_for_expectation_because_it_does_need_verifyng
     expectation = new_expectation
-    expectation.invoke
+    invoke(expectation)
     assertion_counter = SimpleCounter.new
     expectation.verified?(assertion_counter)
     assert_equal 1, assertion_counter.count
@@ -382,19 +386,19 @@ class ExpectationTest < Mocha::TestCase
 
   def test_should_be_satisfied_when_required_invocation_has_been_made
     expectation = Expectation.new(nil, :method_one).times(1)
-    expectation.invoke
+    invoke(expectation)
     assert expectation.satisfied?
   end
 
   def test_should_not_be_satisfied_when_minimum_number_of_invocations_has_not_been_made
     expectation = Expectation.new(nil, :method_one).at_least(2)
-    expectation.invoke
+    invoke(expectation)
     assert !expectation.satisfied?
   end
 
   def test_should_be_satisfied_when_minimum_number_of_invocations_has_been_made
     expectation = Expectation.new(nil, :method_one).at_least(2)
-    2.times { expectation.invoke }
+    2.times { invoke(expectation) }
     assert expectation.satisfied?
   end
 
@@ -439,7 +443,7 @@ class ExpectationTest < Mocha::TestCase
 
     expectation.then(state)
 
-    expectation.invoke
+    invoke(expectation)
     assert state.active?
   end
 
