b34726e09bd83e4267446503e7416dacc0562611
Nitish Rathi <nitishrathi@gmail.com>
2019-11-30 09:55:29 +0000

    substitute algorithm for ArgumentIterator.each
    Array will return a single-element array in case the arg is a single value, or a
    Hash converted to an array in case the arg is a Hash. We then yield the single
    element or the key value pair, and return the result of the last yield

--- lib/mocha/argument_iterator.rb
+++ lib/mocha/argument_iterator.rb
@@ -1,13 +1,7 @@
 module Mocha
   class ArgumentIterator
     def self.each(argument)
-      if argument.is_a?(Hash)
-        argument.each do |method_name, return_value|
-          yield method_name, return_value
-        end
-      else
-        yield argument
-      end
+      Array(argument).map { |*args| yield *(args.flatten) }.last
     end
   end
 end
