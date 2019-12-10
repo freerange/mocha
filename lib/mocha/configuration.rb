7a60ebcba5013d54d8e8bbd39a891ebd85d174c0
James Mead <james@floehopper.org>
2019-12-10 13:44:35 +0000

    Add config option for retaining undocumented behaviour from v1.9

--- lib/mocha/configuration.rb
+++ lib/mocha/configuration.rb
@@ -42,7 +42,8 @@ module Mocha
       :stubbing_non_existent_method => :allow,
       :stubbing_non_public_method => :allow,
       :stubbing_method_on_nil => :prevent,
-      :display_matching_invocations_on_failure => false
+      :display_matching_invocations_on_failure => false,
+      :reinstate_undocumented_behaviour_from_v1_9 => false
     }.freeze
 
     attr_reader :options
@@ -245,6 +246,20 @@ module Mocha
     # @private
     def display_matching_invocations_on_failure?
       @options[:display_matching_invocations_on_failure]
+    end
+
+    # Reinstate undocumented behaviour from v1.9
+    #
+    # Enabling this configuration option reinstates the previous behaviour, but displays a deprecation warning.
+    #
+    # @param [Boolean] value +true+ to reinstate undocumented behaviour; disabled by default.
+    def reinstate_undocumented_behaviour_from_v1_9=(value)
+      @options[:reinstate_undocumented_behaviour_from_v1_9] = value
+    end
+
+    # @private
+    def reinstate_undocumented_behaviour_from_v1_9?
+      @options[:reinstate_undocumented_behaviour_from_v1_9]
     end
 
     class << self
