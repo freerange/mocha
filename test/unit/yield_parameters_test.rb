087aca9bea914e79c3c661dba54a1d0ea5cbbd80
Nitish Rathi <nitishrathi@gmail.com>
2019-12-11 20:51:02 +0000

    Extract assertion method to DRY up YieldParametersTest

--- test/unit/yield_parameters_test.rb
+++ test/unit/yield_parameters_test.rb
@@ -8,70 +8,65 @@ class YieldParametersTest < Mocha::TestCase
 
   def test_should_return_null_yield_parameter_group_by_default
     yield_parameters = YieldParameters.new
-    assert_equal [], yield_parameters.next_invocation.parameter_groups
+    assert_next_invocation_yields(yield_parameters, [])
   end
 
   def test_should_return_single_yield_parameter_group
     yield_parameters = YieldParameters.new
     yield_parameters.multiple_add([1, 2, 3])
-    parameter_group = yield_parameters.next_invocation
-    assert_equal [[1, 2, 3]], parameter_group.parameter_groups
+    assert_next_invocation_yields(yield_parameters, [[1, 2, 3]])
   end
 
   def test_should_keep_returning_single_yield_parameter_group
     yield_parameters = YieldParameters.new
     yield_parameters.multiple_add([1, 2, 3])
     yield_parameters.next_invocation
-    parameter_group = yield_parameters.next_invocation
-    assert_equal [[1, 2, 3]], parameter_group.parameter_groups
-    parameter_group = yield_parameters.next_invocation
-    assert_equal [[1, 2, 3]], parameter_group.parameter_groups
+    assert_next_invocation_yields(yield_parameters, [[1, 2, 3]])
+    assert_next_invocation_yields(yield_parameters, [[1, 2, 3]])
   end
 
   def test_should_return_consecutive_single_yield_parameter_groups
     yield_parameters = YieldParameters.new
     yield_parameters.multiple_add([1, 2, 3])
     yield_parameters.multiple_add([4, 5])
-    parameter_group = yield_parameters.next_invocation
-    assert_equal [[1, 2, 3]], parameter_group.parameter_groups
-    parameter_group = yield_parameters.next_invocation
-    assert_equal [[4, 5]], parameter_group.parameter_groups
+    assert_next_invocation_yields(yield_parameters, [[1, 2, 3]])
+    assert_next_invocation_yields(yield_parameters, [[4, 5]])
   end
 
   def test_should_return_multiple_yield_parameter_group
     yield_parameters = YieldParameters.new
     yield_parameters.multiple_add([1, 2, 3], [4, 5])
-    parameter_group = yield_parameters.next_invocation
-    assert_equal [[1, 2, 3], [4, 5]], parameter_group.parameter_groups
+    assert_next_invocation_yields(yield_parameters, [[1, 2, 3], [4, 5]])
   end
 
   def test_should_keep_returning_multiple_yield_parameter_group
     yield_parameters = YieldParameters.new
     yield_parameters.multiple_add([1, 2, 3], [4, 5])
     yield_parameters.next_invocation
-    parameter_group = yield_parameters.next_invocation
-    assert_equal [[1, 2, 3], [4, 5]], parameter_group.parameter_groups
-    parameter_group = yield_parameters.next_invocation
-    assert_equal [[1, 2, 3], [4, 5]], parameter_group.parameter_groups
+    assert_next_invocation_yields(yield_parameters, [[1, 2, 3], [4, 5]])
+    assert_next_invocation_yields(yield_parameters, [[1, 2, 3], [4, 5]])
   end
 
   def test_should_return_consecutive_multiple_yield_parameter_groups
     yield_parameters = YieldParameters.new
     yield_parameters.multiple_add([1, 2, 3], [4, 5])
     yield_parameters.multiple_add([6, 7], [8, 9, 0])
-    parameter_group = yield_parameters.next_invocation
-    assert_equal [[1, 2, 3], [4, 5]], parameter_group.parameter_groups
-    parameter_group = yield_parameters.next_invocation
-    assert_equal [[6, 7], [8, 9, 0]], parameter_group.parameter_groups
+    assert_next_invocation_yields(yield_parameters, [[1, 2, 3], [4, 5]])
+    assert_next_invocation_yields(yield_parameters, [[6, 7], [8, 9, 0]])
   end
 
   def test_should_return_consecutive_single_and_multiple_yield_parameter_groups
     yield_parameters = YieldParameters.new
     yield_parameters.multiple_add([1, 2, 3])
     yield_parameters.multiple_add([4, 5, 6], [7, 8])
+    assert_next_invocation_yields(yield_parameters, [[1, 2, 3]])
+    assert_next_invocation_yields(yield_parameters, [[4, 5, 6], [7, 8]])
+  end
+
+  private
+
+  def assert_next_invocation_yields(yield_parameters, expected)
     parameter_group = yield_parameters.next_invocation
-    assert_equal [[1, 2, 3]], parameter_group.parameter_groups
-    parameter_group = yield_parameters.next_invocation
-    assert_equal [[4, 5, 6], [7, 8]], parameter_group.parameter_groups
+    assert_equal expected, parameter_group.parameter_groups
   end
 end
