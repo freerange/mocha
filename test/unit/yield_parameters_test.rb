2ce6c93caaf5549724e3bd05c70c2d10f4ef526c
Nitish Rathi <nitishrathi@gmail.com>
2019-12-11 08:36:17 +0000

    Inline YieldParameters#add

--- test/unit/yield_parameters_test.rb
+++ test/unit/yield_parameters_test.rb
@@ -15,14 +15,14 @@ class YieldParametersTest < Mocha::TestCase
 
   def test_should_return_single_yield_parameter_group
     yield_parameters = YieldParameters.new
-    yield_parameters.add(1, 2, 3)
+    yield_parameters.multiple_add([1, 2, 3])
     parameter_group = yield_parameters.next_invocation
     assert_equal [[1, 2, 3]], parameter_group.parameter_groups
   end
 
   def test_should_keep_returning_single_yield_parameter_group
     yield_parameters = YieldParameters.new
-    yield_parameters.add(1, 2, 3)
+    yield_parameters.multiple_add([1, 2, 3])
     yield_parameters.next_invocation
     parameter_group = yield_parameters.next_invocation
     assert_equal [[1, 2, 3]], parameter_group.parameter_groups
@@ -32,8 +32,8 @@ class YieldParametersTest < Mocha::TestCase
 
   def test_should_return_consecutive_single_yield_parameter_groups
     yield_parameters = YieldParameters.new
-    yield_parameters.add(1, 2, 3)
-    yield_parameters.add(4, 5)
+    yield_parameters.multiple_add([1, 2, 3])
+    yield_parameters.multiple_add([4, 5])
     parameter_group = yield_parameters.next_invocation
     assert_equal [[1, 2, 3]], parameter_group.parameter_groups
     parameter_group = yield_parameters.next_invocation
@@ -74,7 +74,7 @@ class YieldParametersTest < Mocha::TestCase
 
   def test_should_return_consecutive_single_and_multiple_yield_parameter_groups
     yield_parameters = YieldParameters.new
-    yield_parameters.add(1, 2, 3)
+    yield_parameters.multiple_add([1, 2, 3])
     yield_parameters.multiple_add([4, 5, 6], [7, 8])
     parameter_group = yield_parameters.next_invocation
     assert_equal [[1, 2, 3]], parameter_group.parameter_groups
