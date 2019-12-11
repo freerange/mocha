c002e857e42fccf398be424d22a4ae548eb399cb
Nitish Rathi <nitishrathi@gmail.com>
2019-12-11 20:58:52 +0000

    Inline MultipleYields#each

--- lib/mocha/multiple_yields.rb
+++ lib/mocha/multiple_yields.rb
@@ -5,11 +5,5 @@ module Mocha
     def initialize(*parameter_groups)
       @parameter_groups = parameter_groups
     end
-
-    def each
-      @parameter_groups.each do |parameter_group|
-        yield(parameter_group)
-      end
-    end
   end
 end
