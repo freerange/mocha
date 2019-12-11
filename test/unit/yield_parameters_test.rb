86f33a8300e1d9fb010532f060de5177a28d101d
Nitish Rathi <nitishrathi@gmail.com>
2019-12-11 09:07:01 +0000

    delete unused NoYields

--- test/unit/yield_parameters_test.rb
+++ test/unit/yield_parameters_test.rb
@@ -1,7 +1,6 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 require 'mocha/yield_parameters'
-require 'mocha/no_yields'
 require 'mocha/multiple_yields'
 
 class YieldParametersTest < Mocha::TestCase
