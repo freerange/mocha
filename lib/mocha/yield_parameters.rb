41731a4fdfaecc2d46c94f2be7a6baabd58a5ab5
Nitish Rathi <nitishrathi@gmail.com>
2019-12-11 08:54:51 +0000

    Use MultipleYields instead of NoYields
    By default an Expectation and hence a YieldParameters is not meant to
    attempt to yield to a matching invocation.
    
    The NoYields functionality is just a special case of the MultipleYields
    functionality, i.e. one with no parameter groups.

--- lib/mocha/yield_parameters.rb
+++ lib/mocha/yield_parameters.rb
@@ -9,7 +9,7 @@ module Mocha
 
     def next_invocation
       case @parameter_groups.length
-      when 0 then NoYields.new
+      when 0 then MultipleYields.new
       when 1 then @parameter_groups.first
       else @parameter_groups.shift
       end
