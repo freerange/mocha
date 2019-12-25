6d6a8b9beb077530e02ba349e7e8c474bc61d4e8
Nitish Rathi <nitishrathi@gmail.com>
2019-12-25 16:49:40 +0000

    move stubbing_method_unnecessarily check outside loop

--- lib/mocha/mockery.rb
+++ lib/mocha/mockery.rb
@@ -80,8 +80,8 @@ module Mocha
         backtrace = unsatisfied_expectations.empty? ? caller : unsatisfied_expectations[0].backtrace
         raise ExpectationErrorFactory.build("not all expectations were satisfied\n#{mocha_inspect}", backtrace)
       end
-      expectations.each do |e|
-        unless Mocha.configuration.stubbing_method_unnecessarily == :allow
+      unless Mocha.configuration.stubbing_method_unnecessarily == :allow
+        expectations.each do |e|
           next if e.used?
           on_stubbing_method_unnecessarily(e)
         end
