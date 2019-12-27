98e70031b2433e62ae93f578805caeb331d79fe0
Nitish Rathi <nitishrathi@gmail.com>
2019-12-27 23:14:44 +0000

    merge Fake{,AnyInstance}Class into FakeClass

--- test/unit/receivers_test.rb
+++ test/unit/receivers_test.rb
@@ -1,8 +1,41 @@
 require File.expand_path('../../test_helper', __FILE__)
 require 'mocha/receivers'
+module ReceiversTest
+  class FakeClass
+    class AnyInstance
+      def initialize(mocha)
+        @mocha = mocha
+      end
+
+      def mocha(_instantiate)
+        @mocha
+      end
+    end
+
+    attr_reader :superclass
+
+    def initialize(superclass, mocha)
+      @superclass = superclass
+      @mocha = mocha
+    end
+
+    def mocha(_instantiate)
+      @mocha
+    end
+
+    def any_instance
+      AnyInstance.new(@mocha)
+    end
+
+    def is_a?(_klass)
+      true
+    end
+  end
+end
 
 class InstanceReceiverTest < Mocha::TestCase
   include Mocha
+  include ReceiversTest
 
   class FakeObject
     def initialize(mocha)
@@ -15,23 +48,6 @@ class InstanceReceiverTest < Mocha::TestCase
 
     def is_a?(_klass)
       false
-    end
-  end
-
-  class FakeClass
-    attr_reader :superclass
-
-    def initialize(superclass, mocha)
-      @superclass = superclass
-      @mocha = mocha
-    end
-
-    def mocha(_instantiate)
-      @mocha
-    end
-
-    def is_a?(klass)
-      klass == Class
     end
   end
 
@@ -52,38 +68,12 @@ end
 
 class AnyInstanceReceiverTest < Mocha::TestCase
   include Mocha
-
-  class FakeAnyInstanceClass
-    class AnyInstance
-      def initialize(mocha)
-        @mocha = mocha
-      end
-
-      def mocha(_instantiate)
-        @mocha
-      end
-    end
-
-    attr_reader :superclass
-
-    def initialize(superclass, mocha)
-      @superclass = superclass
-      @mocha = mocha
-    end
-
-    def any_instance
-      AnyInstance.new(@mocha)
-    end
-
-    def is_a?(_klass)
-      true
-    end
-  end
+  include ReceiversTest
 
   def test_mocks_returns_mocks_for_class_and_its_superclasses
-    grandparent = FakeAnyInstanceClass.new(nil, :grandparent_mocha)
-    parent = FakeAnyInstanceClass.new(grandparent, :parent_mocha)
-    klass = FakeAnyInstanceClass.new(parent, :mocha)
+    grandparent = FakeClass.new(nil, :grandparent_mocha)
+    parent = FakeClass.new(grandparent, :parent_mocha)
+    klass = FakeClass.new(parent, :mocha)
     receiver = AnyInstanceReceiver.new(klass)
     assert_equal [:mocha, :parent_mocha, :grandparent_mocha], receiver.mocks
   end
