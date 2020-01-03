a155cf906c4821e3798fa70849a0a1f4ef61f416
Nitish Rathi <nitishrathi@gmail.com>
2020-01-03 22:26:03 +0000

    reuse StubInstanceMethodSharedTests in StubMethodDefinedOnModuleAndAliasedTest

--- test/acceptance/stub_method_defined_on_module_and_aliased_test.rb
+++ test/acceptance/stub_method_defined_on_module_and_aliased_test.rb
@@ -1,38 +1,19 @@
-require File.expand_path('../acceptance_test_helper', __FILE__)
+require File.expand_path('../stub_instance_method_shared_tests', __FILE__)
 
 unless Mocha::PRE_RUBY_V19
   class StubMethodDefinedOnModuleAndAliasedTest < Mocha::TestCase
-    include AcceptanceTest
+    include StubInstanceMethodSharedTests
 
-    def setup
-      setup_acceptance_test
+    def method_owner
+      @method_owner ||= Module.new
     end
 
-    def teardown
-      teardown_acceptance_test
+    def stubbed_instance
+      Class.new.send(:extend, method_owner)
     end
 
-    def test_stubbing_class_method_defined_by_aliasing_module_instance_method
-      mod = Module.new do
-        def module_instance_method
-          'module-instance-method'
-        end
-      end
-
-      klass = Class.new do
-        extend mod
-        class << self
-          alias_method :aliased_module_instance_method, :module_instance_method
-        end
-      end
-
-      assert_snapshot_unchanged(klass) do
-        test_result = run_as_test do
-          klass.stubs(:aliased_module_instance_method).returns('stubbed-aliased-module-instance-method')
-          assert_equal 'stubbed-aliased-module-instance-method', klass.aliased_module_instance_method
-        end
-        assert_passed(test_result)
-      end
+    def stub_aliased_method
+      true
     end
   end
 end
