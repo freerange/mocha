e9cfc66181aee36d48d9c757f4c91fad840ba077
Nitish Rathi <nitishrathi@gmail.com>
2020-01-03 19:01:17 +0000

    split StubInstanceMethodDefinedOnKernelModuleTest into two
    This allows us to reuse StubInstanceMethodSharedTests for checking stubbing on a
    Module and stubbing on an object

--- test/acceptance/stub_instance_method_defined_on_kernel_module_test.rb
+++ test/acceptance/stub_instance_method_defined_on_kernel_module_test.rb
@@ -1,8 +1,6 @@
 require File.expand_path('../stub_instance_method_shared_tests', __FILE__)
 
-class StubInstanceMethodDefinedOnKernelModuleTest < Mocha::TestCase
-  include StubInstanceMethodSharedTests
-
+class StubInstanceMethodDefinedOnKernelStubbedOnAnObjectTest < Mocha::TestCase
   def method_owner
     Kernel
   end
@@ -10,16 +8,14 @@ class StubInstanceMethodDefinedOnKernelModuleTest < Mocha::TestCase
   def stubbed_class
     Class.new
   end
+end
 
-  def test_should_stub_public_module_method_and_leave_it_unchanged_after_test
-    assert_snapshot_unchanged_on_stubbing(:public, Module.new)
+class StubInstanceMethodDefinedOnKernelStubbedOnAModuleTest < Mocha::TestCase
+  def method_owner
+    Kernel
   end
 
-  def test_should_stub_protected_module_method_and_leave_it_unchanged_after_test
-    assert_snapshot_unchanged_on_stubbing(:protected, Module.new)
-  end
-
-  def test_should_stub_private_module_method_and_leave_it_unchanged_after_test
-    assert_snapshot_unchanged_on_stubbing(:private, Module.new)
+  def stubbed_class
+    Module
   end
 end
