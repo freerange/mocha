a84b186ec3bfd7a43a5cf756677b752d22a4db99
Nitish Rathi <nitishrathi@gmail.com>
2020-01-03 19:21:55 +0000

    reuse StubInstanceMethodSharedTests in StubInstanceMethodDefinedOnSingletonClass

--- test/acceptance/stub_instance_method_defined_on_singleton_class_test.rb
+++ test/acceptance/stub_instance_method_defined_on_singleton_class_test.rb
@@ -1,67 +1,13 @@
-require File.expand_path('../acceptance_test_helper', __FILE__)
+require File.expand_path('../stub_instance_method_shared_tests', __FILE__)
 
 class StubInstanceMethodDefinedOnSingletonClassTest < Mocha::TestCase
-  include AcceptanceTest
+  include StubInstanceMethodSharedTests
 
-  def setup
-    setup_acceptance_test
+  def method_owner
+    stubbed_instance.singleton_class
   end
 
-  def teardown
-    teardown_acceptance_test
-  end
-
-  def test_should_stub_public_method_and_leave_it_unchanged_after_test
-    instance = Class.new.new
-    class << instance
-      def my_singleton_method
-        :original_return_value
-      end
-      public :my_singleton_method
-    end
-    assert_snapshot_unchanged(instance) do
-      test_result = run_as_test do
-        instance.stubs(:my_singleton_method).returns(:stubbed_return_value)
-        assert_equal :stubbed_return_value, instance.my_singleton_method
-      end
-      assert_passed(test_result)
-    end
-    assert_equal :original_return_value, instance.my_singleton_method
-  end
-
-  def test_should_stub_protected_method_and_leave_it_unchanged_after_test
-    instance = Class.new.new
-    class << instance
-      def my_singleton_method
-        :original_return_value
-      end
-      protected :my_singleton_method
-    end
-    assert_snapshot_unchanged(instance) do
-      test_result = run_as_test do
-        instance.stubs(:my_singleton_method).returns(:stubbed_return_value)
-        assert_equal :stubbed_return_value, instance.send(:my_singleton_method)
-      end
-      assert_passed(test_result)
-    end
-    assert_equal :original_return_value, instance.send(:my_singleton_method)
-  end
-
-  def test_should_stub_private_method_and_leave_it_unchanged_after_test
-    instance = Class.new.new
-    class << instance
-      def my_singleton_method
-        :original_return_value
-      end
-      private :my_singleton_method
-    end
-    assert_snapshot_unchanged(instance) do
-      test_result = run_as_test do
-        instance.stubs(:my_singleton_method).returns(:stubbed_return_value)
-        assert_equal :stubbed_return_value, instance.send(:my_singleton_method)
-      end
-      assert_passed(test_result)
-    end
-    assert_equal :original_return_value, instance.send(:my_singleton_method)
+  def stubbed_instance
+    @stubbed_instance ||= Class.new.new
   end
 end
