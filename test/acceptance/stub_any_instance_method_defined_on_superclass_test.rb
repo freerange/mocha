620ff247462b76f4f82348142a2c818627939ab8
Nitish Rathi <nitishrathi@gmail.com>
2020-01-06 09:20:48 +0000

    use shared tests for AnyInstance superclass checks
    ... while moving them from stub_any_instance_method_test to
    stub_any_instance_method_defined_on_superclass_test

--- test/acceptance/stub_any_instance_method_defined_on_superclass_test.rb
+++ test/acceptance/stub_any_instance_method_defined_on_superclass_test.rb
@@ -1,33 +1,18 @@
-require File.expand_path('../acceptance_test_helper', __FILE__)
+require File.expand_path('../stub_method_shared_tests', __FILE__)
 
 class StubAnyInstanceMethodDefinedOnSuperclassTest < Mocha::TestCase
-  include AcceptanceTest
+  include StubMethodSharedTests
 
-  def setup
-    setup_acceptance_test
+  def method_owner
+    stubbed_instance.class.superclass
   end
 
-  def teardown
-    teardown_acceptance_test
+  def stubbed_instance
+    @stubbed_instance ||= Class.new(Class.new).new
   end
 
-  def test_should_stub_method_and_leave_it_unchanged_after_test
-    superklass = Class.new do
-      def my_superclass_method
-        :original_return_value
-      end
-      public :my_superclass_method
-    end
-    klass = Class.new(superklass)
-    instance = klass.new
-    assert_snapshot_unchanged(instance) do
-      test_result = run_as_test do
-        superklass.any_instance.stubs(:my_superclass_method).returns(:new_return_value)
-        assert_equal :new_return_value, instance.my_superclass_method
-      end
-      assert_passed(test_result)
-    end
-    assert_equal :original_return_value, instance.my_superclass_method
+  def stub_owner
+    stubbed_instance.class.any_instance
   end
 
   def test_expect_method_on_any_instance_of_superclass_even_if_preceded_by_test_expecting_method_on_any_instance_of_subclass
@@ -60,5 +45,21 @@ class StubAnyInstanceMethodDefinedOnSuperclassTest < Mocha::TestCase
       'unsatisfied expectations:',
       '- expected exactly once, invoked never: #<AnyInstance:superklass>.my_instance_method(any_parameters)'
     ], test_result.failure_message_lines
+  end
+end
+
+class StubSuperclassAnyInstanceMethodDefinedOnSuperclassTest < Mocha::TestCase
+  include StubMethodSharedTests
+
+  def method_owner
+    stubbed_instance.class.superclass
+  end
+
+  def stubbed_instance
+    @stubbed_instance ||= Class.new(Class.new).new
+  end
+
+  def stub_owner
+    method_owner.any_instance
   end
 end
