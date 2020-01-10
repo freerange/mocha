ad2cd32fdd9157865b1237147a7f02b1ac8960bb
Nitish Rathi <nitishrathi@gmail.com>
2020-01-10 11:02:03 +0000

    use shared tests in StubbingOnNonMockObjectTest
    ... just as in all the StubbingNonExistent*MethodTest

--- test/acceptance/stubbing_on_non_mock_object_test.rb
+++ test/acceptance/stubbing_on_non_mock_object_test.rb
@@ -1,62 +1,29 @@
-require File.expand_path('../acceptance_test_helper', __FILE__)
-require 'mocha/configuration'
+require File.expand_path('../stubbing_with_potential_violation_shared_tests', __FILE__)
 
 class StubbingOnNonMockObjectTest < Mocha::TestCase
-  include AcceptanceTest
+  include StubbingWithPotentialViolationSharedTests
 
   def setup
-    setup_acceptance_test
-  end
-
-  def teardown
-    teardown_acceptance_test
-  end
-
-  def test_should_allow_stubbing_method_on_non_mock_object
-    Mocha.configure { |c| c.stubbing_method_on_non_mock_object = :allow }
-    non_mock_object = Class.new do
+    super
+    @non_mock_object = Class.new do
       def existing_method; end
     end
-    test_result = run_as_test do
-      non_mock_object.stubs(:existing_method)
-    end
-    assert_passed(test_result)
-    assert !@logger.warnings.include?("stubbing method on non-mock object: #{non_mock_object.mocha_inspect}.existing_method")
   end
 
-  def test_should_warn_on_stubbing_method_on_non_mock_object
-    Mocha.configure { |c| c.stubbing_method_on_non_mock_object = :warn }
-    non_mock_object = Class.new do
-      def existing_method; end
-    end
-    test_result = run_as_test do
-      non_mock_object.stubs(:existing_method)
-    end
-    assert_passed(test_result)
-    assert @logger.warnings.include?("stubbing method on non-mock object: #{non_mock_object.mocha_inspect}.existing_method")
+  def configure_violation(config, treatment)
+    config.stubbing_method_on_non_mock_object = treatment
   end
 
-  def test_should_prevent_stubbing_method_on_non_mock_object
-    Mocha.configure { |c| c.stubbing_method_on_non_mock_object = :prevent }
-    non_mock_object = Class.new do
-      def existing_method; end
-    end
-    test_result = run_as_test do
-      non_mock_object.stubs(:existing_method)
-    end
-    assert_failed(test_result)
-    assert test_result.error_messages.include?("Mocha::StubbingError: stubbing method on non-mock object: #{non_mock_object.mocha_inspect}.existing_method")
+  def potential_violation
+    @non_mock_object.stubs(:existing_method)
+  end
+
+  def message_on_violation
+    "stubbing method on non-mock object: #{@non_mock_object.mocha_inspect}.existing_method"
   end
 
   def test_should_default_to_allow_stubbing_method_on_non_mock_object
-    non_mock_object = Class.new do
-      def existing_method; end
-    end
-    test_result = run_as_test do
-      non_mock_object.stubs(:existing_method)
-    end
-    assert_passed(test_result)
-    assert !@logger.warnings.include?("stubbing method on non-mock object: #{non_mock_object.mocha_inspect}.existing_method")
+    assert_defaults_to_allow_stubbing_with_potential_violation
   end
 
   def test_should_allow_stubbing_method_on_mock_object
