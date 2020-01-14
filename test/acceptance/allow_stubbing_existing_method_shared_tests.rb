702992f1f958c1b6d785352ec7c58fcccb2466db
Nitish Rathi <nitishrathi@gmail.com>
2020-01-14 08:56:57 +0000

    move mod to own file for use in class method tests

---
+++ test/acceptance/allow_stubbing_existing_method_shared_tests.rb
@@ -0,0 +1,36 @@
+require File.expand_path('../acceptance_test_helper', __FILE__)
+
+module AllowStubbingExistingMethodSharedTests
+  include AcceptanceTest
+
+  def setup
+    setup_acceptance_test
+  end
+
+  def teardown
+    teardown_acceptance_test
+  end
+
+  def test_should_allow_stubbing_existing_public_method
+    assert_allows_stubbing_existing_method(:public)
+  end
+
+  def test_should_allow_stubbing_existing_protected_method
+    assert_allows_stubbing_existing_method(:protected)
+  end
+
+  def test_should_allow_stubbing_existing_private_method
+    assert_allows_stubbing_existing_method(:private)
+  end
+
+  def assert_allows_stubbing_existing_method(visibility)
+    Mocha.configure { |c| c.stubbing_non_existent_method = :prevent }
+    method_owner.send(:define_method, :existing_method) {}
+    method_owner.send(visibility, :existing_method)
+    stub_owner_in_scope = stub_owner
+    test_result = run_as_test do
+      stub_owner_in_scope.stubs(:existing_method)
+    end
+    assert_passed(test_result)
+  end
+end
--- test/acceptance/stubbing_non_existent_any_instance_method_test.rb
+++ test/acceptance/stubbing_non_existent_any_instance_method_test.rb
@@ -1,4 +1,5 @@
 require File.expand_path('../stubbing_with_potential_violation_shared_tests', __FILE__)
+require File.expand_path('../allow_stubbing_existing_method_shared_tests', __FILE__)
 
 class StubbingNonExistentAnyInstanceMethodTest < Mocha::TestCase
   include StubbingWithPotentialViolationDefaultingToAllowedSharedTests
@@ -18,41 +19,6 @@ class StubbingNonExistentAnyInstanceMethodTest < Mocha::TestCase
 
   def message_on_violation
     "stubbing non-existent method: #{@klass.any_instance.mocha_inspect}.non_existent_method"
-  end
-end
-
-module AllowStubbingExistingMethodSharedTests
-  include AcceptanceTest
-
-  def setup
-    setup_acceptance_test
-  end
-
-  def teardown
-    teardown_acceptance_test
-  end
-
-  def test_should_allow_stubbing_existing_public_method
-    assert_allows_stubbing_existing_method(:public)
-  end
-
-  def test_should_allow_stubbing_existing_protected_method
-    assert_allows_stubbing_existing_method(:protected)
-  end
-
-  def test_should_allow_stubbing_existing_private_method
-    assert_allows_stubbing_existing_method(:private)
-  end
-
-  def assert_allows_stubbing_existing_method(visibility)
-    Mocha.configure { |c| c.stubbing_non_existent_method = :prevent }
-    method_owner.send(:define_method, :existing_method) {}
-    method_owner.send(visibility, :existing_method)
-    stub_owner_in_scope = stub_owner
-    test_result = run_as_test do
-      stub_owner_in_scope.stubs(:existing_method)
-    end
-    assert_passed(test_result)
   end
 end
 
