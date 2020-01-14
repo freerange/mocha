dae21233d50b9302edc6d98109dd9f927f04ada7
Nitish Rathi <nitishrathi@gmail.com>
2020-01-14 17:01:07 +0000

    Refactor: extract assert_allows_stubbing_method

--- test/acceptance/allow_stubbing_existing_method_shared_tests.rb
+++ test/acceptance/allow_stubbing_existing_method_shared_tests.rb
@@ -27,21 +27,20 @@ module AllowStubbingExistingMethodSharedTests
     method_owner.send(:define_method, :respond_to?) do |method|
       (method == :method_responded_to)
     end
-    Mocha.configure { |c| c.stubbing_non_existent_method = :prevent }
-    stub_owner_in_scope = stub_owner
-    test_result = run_as_test do
-      stub_owner_in_scope.stubs(:method_responded_to)
-    end
-    assert_passed(test_result)
+    assert_allows_stubbing_method(:method_responded_to)
   end
 
   def assert_allows_stubbing_existing_method(visibility)
     method_owner.send(:define_method, :existing_method) {}
     method_owner.send(visibility, :existing_method)
+    assert_allows_stubbing_method(:existing_method)
+  end
+
+  def assert_allows_stubbing_method(stubbed_method)
     Mocha.configure { |c| c.stubbing_non_existent_method = :prevent }
     stub_owner_in_scope = stub_owner
     test_result = run_as_test do
-      stub_owner_in_scope.stubs(:existing_method)
+      stub_owner_in_scope.stubs(stubbed_method)
     end
     assert_passed(test_result)
   end
