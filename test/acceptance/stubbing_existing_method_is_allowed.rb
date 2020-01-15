8f6a9b494d34dcbd13e4671e9225c6a99c0c715b
Nitish Rathi <nitishrathi@gmail.com>
2020-01-15 15:20:45 +0000

    use StubbingPublicMethodIsAllowed in StubbingExistingMethodIsAllowed

--- test/acceptance/stubbing_existing_method_is_allowed.rb
+++ test/acceptance/stubbing_existing_method_is_allowed.rb
@@ -1,19 +1,7 @@
-require File.expand_path('../acceptance_test_helper', __FILE__)
+require File.expand_path('../stubbing_public_method_is_allowed', __FILE__)
 
 module StubbingExistingMethodIsAllowed
-  include AcceptanceTest
-
-  def setup
-    setup_acceptance_test
-  end
-
-  def teardown
-    teardown_acceptance_test
-  end
-
-  def test_should_allow_stubbing_existing_public_method
-    assert_allows_stubbing_existing_method(:public)
-  end
+  include StubbingPublicMethodIsAllowed
 
   def test_should_allow_stubbing_existing_protected_method
     assert_allows_stubbing_existing_method(:protected)
@@ -23,26 +11,10 @@ module StubbingExistingMethodIsAllowed
     assert_allows_stubbing_existing_method(:private)
   end
 
-  def test_should_allow_stubbing_method_responded_to
-    method_owner.send(:define_method, :respond_to?) do |method|
-      (method == :method_responded_to)
-    end
-    assert_allows_stubbing_method(:method_responded_to)
-  end
-
   def assert_allows_stubbing_existing_method(visibility)
     method_owner.send(:define_method, :existing_method) {}
     method_owner.send(visibility, :existing_method)
     assert_allows_stubbing_method(:existing_method)
-  end
-
-  def assert_allows_stubbing_method(stubbed_method)
-    Mocha.configure { |c| configure_violation(c, :prevent) }
-    stub_owner_in_scope = stub_owner
-    test_result = run_as_test do
-      stub_owner_in_scope.stubs(stubbed_method)
-    end
-    assert_passed(test_result)
   end
 
   def configure_violation(config, treatment)
