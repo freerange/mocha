a87d3488de0f19f228e3447172d6efb8026fc6c9
James Mead <james@floehopper.org>
2022-04-20 17:41:39 +0100

    Mock#expects,#stubs should return last expectation
    Even when passed a hash of method names vs return values. This is the
    same behaviour as for ObjectMethods#expects & #stubs and the behaviour
    described by the documentation for Mock#expects & #stubs.
    
    Fixes #524.

---
+++ test/acceptance/issue_524_test.rb
@@ -0,0 +1,35 @@
+require File.expand_path('../acceptance_test_helper', __FILE__)
+
+class Issue524Test < Mocha::TestCase
+  include AcceptanceTest
+
+  def setup
+    setup_acceptance_test
+  end
+
+  def teardown
+    teardown_acceptance_test
+  end
+
+  def test_expects_returns_last_expectation
+    test_result = run_as_test do
+      object = mock
+      object.expects(:method_1 => 1, :method_2 => 2).twice
+      object.method_1
+      object.method_2
+      object.method_2
+    end
+    assert_passed(test_result)
+  end
+
+  def test_stubs_returns_last_expectation
+    test_result = run_as_test do
+      object = mock
+      object.stubs(:method_1 => 1, :method_2 => 2).twice
+      object.method_1
+      object.method_2
+      object.method_2
+    end
+    assert_passed(test_result)
+  end
+end
