6b678aef7fb833cff01e9a4ad8c8d3f6656fa26b
Nitish Rathi <nitish.rathi@simplybusiness.co.uk>
2022-08-30 15:05:56 +0100

    Remove redundant to_sym in Assertions
    Reflection-related methods returning arrays of method names, e.g.
    Object#public_methods, Module#public_instance_methods, etc, seem to have
    changed from returning arrays of strings in Ruby v1.8 to returning
    arrays of symbols in Ruby v1.9. See also
    970502896d8e27696bf66886e84d1e5f5c43984e.
    
    Since we dropped support for Ruby v1.8 in #536, we can safely remove the
    call to to_sym from Assertions#assert_method_visibility.

--- test/assertions.rb
+++ test/assertions.rb
@@ -1,6 +1,5 @@
 module Assertions
   def assert_method_visibility(object, method_name, visiblity)
-    method_key = method_name.to_sym
-    assert object.send("#{visiblity}_methods").include?(method_key), "#{method_name} is not #{visiblity}"
+    assert object.send("#{visiblity}_methods").include?(method_name), "#{method_name} is not #{visiblity}"
   end
 end
