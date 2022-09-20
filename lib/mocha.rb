0e64d10c8f0f9e0143c8fe2c83926a9631e61f45
wasabigeek <wasabigeek@users.noreply.github.com>
2022-09-20 22:22:28 +0800

    Add Ruby < 2.0 deprecation warning

--- lib/mocha.rb
+++ lib/mocha.rb
@@ -1,1 +1,9 @@
 require 'mocha/version'
+require 'mocha/ruby_version'
+require 'mocha/deprecation'
+
+unless Mocha::RUBY_V2_PLUS
+  Mocha::Deprecation.warning(
+    'Versions of Ruby earlier than v2.0 will not be supported in future versions of Mocha.'
+  )
+end
