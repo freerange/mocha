f38be1d463c385f750fd9ca8b635fd57efcde36b
wasabigeek <wasabigeek@users.noreply.github.com>
2022-09-22 23:25:56 +0800

    Remove RUBY_V2_PLUS

--- test/unit/any_instance_method_test.rb
+++ test/unit/any_instance_method_test.rb
@@ -16,17 +16,6 @@ class AnyInstanceMethodTest < Mocha::TestCase
     end
   end
 
-  unless RUBY_V2_PLUS
-    def test_should_hide_original_method
-      klass = class_with_method(:method_x)
-      method = AnyInstanceMethod.new(klass, :method_x)
-
-      method.hide_original_method
-
-      assert_equal false, klass.method_defined?(:method_x)
-    end
-  end
-
   def test_should_not_raise_error_hiding_method_that_isnt_defined
     klass = class_with_method(:irrelevant)
     method = AnyInstanceMethod.new(klass, :method_x)
