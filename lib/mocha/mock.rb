636d4633d2f91a02450f653deb4241d7712a0532
wasabigeek <wasabigeek@users.noreply.github.com>
2022-09-25 22:14:19 +0800

    Mark methods passing keyword args via args splat
    This marks Expectation#with, Mock#method_missing, and the method defined
    by StubbedMethod#define_new_method. See [1,2] for more information about
    how Module#ruby2_keywords works.
    
    The ruby2_keywords shim gem [3] provides an empty Module#ruby2_keywords
    method, for forward source-level compatibility against Ruby v2.7 and
    Ruby v3.
    
    [1]: https://ruby-doc.org/core-3.1.2/Module.html#method-i-ruby2_keywords
    [2]: https://www.ruby-lang.org/en/news/2019/12/12/separation-of-positional-and-keyword-arguments-in-ruby-3-0/
    [3]: https://github.com/ruby/ruby2_keywords

--- lib/mocha/mock.rb
+++ lib/mocha/mock.rb
@@ -1,3 +1,4 @@
+require 'ruby2_keywords'
 require 'mocha/expectation'
 require 'mocha/expectation_list'
 require 'mocha/invocation'
@@ -312,6 +313,7 @@ module Mocha
     def method_missing(symbol, *arguments, &block)
       handle_method_call(symbol, arguments, block)
     end
+    ruby2_keywords(:method_missing)
     # rubocop:enable Style/MethodMissingSuper
 
     # @private
