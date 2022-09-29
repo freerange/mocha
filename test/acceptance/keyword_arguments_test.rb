1bea853bd98bb5d20faa1c9959108fa8453ccc38
Nicholas Koh <nicholas.koh@shopify.com>
2022-09-29 23:36:24 +0800

    Get CI passing for Ruby < 2.7

--- test/acceptance/keyword_arguments_test.rb
+++ test/acceptance/keyword_arguments_test.rb
@@ -194,11 +194,19 @@ class PositionalAndKeywordHashInspectTest < Mocha::TestCase
       mock.expects(:method).with(:foo => 42)
       mock.method(:key => 42) # rubocop:disable Style/BracesAroundHashParameters
     end
-    assert_equal [
-      'unexpected invocation: #<Mock:mock>.method(:key => 42)',
-      'unsatisfied expectations:',
-      '- expected exactly once, invoked never: #<Mock:mock>.method(:foo => 42)'
-    ], test_result.failure_message_lines
+    if Mocha::RUBY_V27_PLUS
+      assert_equal [
+        'unexpected invocation: #<Mock:mock>.method(:key => 42)',
+        'unsatisfied expectations:',
+        '- expected exactly once, invoked never: #<Mock:mock>.method(:foo => 42)'
+      ], test_result.failure_message_lines
+    else
+      assert_equal [
+        'unexpected invocation: #<Mock:mock>.method({:key => 42})',
+        'unsatisfied expectations:',
+        '- expected exactly once, invoked never: #<Mock:mock>.method({:foo => 42})'
+      ], test_result.failure_message_lines
+    end
   end
 
   def test_last_hash_parameters_in_invocation_and_expectation_print_correctly
@@ -220,11 +228,19 @@ class PositionalAndKeywordHashInspectTest < Mocha::TestCase
       mock.expects(:method).with(1, :foo => 42)
       mock.method(1, :key => 42) # rubocop:disable Style/BracesAroundHashParameters
     end
-    assert_equal [
-      'unexpected invocation: #<Mock:mock>.method(1, :key => 42)',
-      'unsatisfied expectations:',
-      '- expected exactly once, invoked never: #<Mock:mock>.method(1, :foo => 42)'
-    ], test_result.failure_message_lines
+    if Mocha::RUBY_V27_PLUS
+      assert_equal [
+        'unexpected invocation: #<Mock:mock>.method(1, :key => 42)',
+        'unsatisfied expectations:',
+        '- expected exactly once, invoked never: #<Mock:mock>.method(1, :foo => 42)'
+      ], test_result.failure_message_lines
+    else
+      assert_equal [
+        'unexpected invocation: #<Mock:mock>.method(1, {:key => 42})',
+        'unsatisfied expectations:',
+        '- expected exactly once, invoked never: #<Mock:mock>.method(1, {:foo => 42})'
+      ], test_result.failure_message_lines
+    end
   end
 
   if Mocha::RUBY_V27_PLUS
