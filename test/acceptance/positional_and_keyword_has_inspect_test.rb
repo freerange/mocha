ef0fa0c9894f31faae260d4656796a2940bb09e5
James Mead <james@floehopper.org>
2022-10-09 15:24:09 +0100

    Improve inspection of positional & keyword Hashes
    With the introduction of strict keyword matching, we're explicitly
    distinguishing between positional and keyword Hashes. However, this
    meant the descriptions of Hash arguments in invocations and parameter
    matchers had become confusing. This attempts to make that less confusing
    by always wrapping a Hash in braces unless we *know* its a keyword Hash,
    in which case don't wrap it in braces.
    
    This means that in earlier versions of Ruby where we can't distinguish
    between a positional and keyword Hash, it will always be displayed
    wrapped in braces.
    
    Co-authored-by: Nicholas Koh <nicholas.koh@shopify.com>

--- test/acceptance/positional_and_keyword_has_inspect_test.rb
+++ test/acceptance/positional_and_keyword_has_inspect_test.rb
@@ -28,11 +28,19 @@ class PositionalAndKeywordHashInspectTest < Mocha::TestCase
       mock.expects(:method).with(:foo => 42)
       mock.method(:key => 42)
     end
-    assert_equal [
-      'unexpected invocation: #<Mock:mock>.method({:key => 42})',
-      'unsatisfied expectations:',
-      '- expected exactly once, invoked never: #<Mock:mock>.method({:foo => 42})'
-    ], test_result.failure_message_lines
+    if Mocha::RUBY_V27_PLUS
+      assert_equal [
+        'unexpected invocation: #<Mock:mock>.method(:key => 42)',
+        'unsatisfied expectations:',
+        '- expected exactly once, invoked never: #<Mock:mock>.method(:foo => 42)'
+      ], test_result.failure_message_lines
+    else
+      assert_equal [
+        'unexpected invocation: #<Mock:mock>.method({:key => 42})',
+        'unsatisfied expectations:',
+        '- expected exactly once, invoked never: #<Mock:mock>.method({:foo => 42})'
+      ], test_result.failure_message_lines
+    end
   end
 
   def test_last_hash_parameters_in_invocation_and_expectation_print_correctly
@@ -54,10 +62,80 @@ class PositionalAndKeywordHashInspectTest < Mocha::TestCase
       mock.expects(:method).with(1, :foo => 42)
       mock.method(1, :key => 42)
     end
-    assert_equal [
-      'unexpected invocation: #<Mock:mock>.method(1, {:key => 42})',
-      'unsatisfied expectations:',
-      '- expected exactly once, invoked never: #<Mock:mock>.method(1, {:foo => 42})'
-    ], test_result.failure_message_lines
+    if Mocha::RUBY_V27_PLUS
+      assert_equal [
+        'unexpected invocation: #<Mock:mock>.method(1, :key => 42)',
+        'unsatisfied expectations:',
+        '- expected exactly once, invoked never: #<Mock:mock>.method(1, :foo => 42)'
+      ], test_result.failure_message_lines
+    else
+      assert_equal [
+        'unexpected invocation: #<Mock:mock>.method(1, {:key => 42})',
+        'unsatisfied expectations:',
+        '- expected exactly once, invoked never: #<Mock:mock>.method(1, {:foo => 42})'
+      ], test_result.failure_message_lines
+    end
+  end
+
+  if Mocha::RUBY_V27_PLUS
+    def test_single_hash_parameters_in_invocation_and_expectation_print_correctly_when_strict_keyword_args_is_enabled
+      test_result = run_as_test do
+        mock = mock('mock')
+        mock.expects(:method).with({ :foo => 42 }) # rubocop:disable Style/BracesAroundHashParameters
+        Mocha::Configuration.override(:strict_keyword_argument_matching => true) do
+          mock.method({ :key => 42 }) # rubocop:disable Style/BracesAroundHashParameters
+        end
+      end
+      assert_equal [
+        'unexpected invocation: #<Mock:mock>.method({:key => 42})',
+        'unsatisfied expectations:',
+        '- expected exactly once, invoked never: #<Mock:mock>.method({:foo => 42})'
+      ], test_result.failure_message_lines
+    end
+
+    def test_unexpected_keyword_arguments_in_invocation_and_expectation_print_correctly_when_strict_keyword_args_is_enabled
+      test_result = run_as_test do
+        mock = mock('mock')
+        mock.expects(:method).with(:foo => 42)
+        Mocha::Configuration.override(:strict_keyword_argument_matching => true) do
+          mock.method(:key => 42)
+        end
+      end
+      assert_equal [
+        'unexpected invocation: #<Mock:mock>.method(:key => 42)',
+        'unsatisfied expectations:',
+        '- expected exactly once, invoked never: #<Mock:mock>.method(:foo => 42)'
+      ], test_result.failure_message_lines
+    end
+
+    def test_last_hash_parameters_in_invocation_and_expectation_print_correctly_when_strict_keyword_args_is_enabled
+      test_result = run_as_test do
+        mock = mock('mock')
+        mock.expects(:method).with(1, { :foo => 42 }) # rubocop:disable Style/BracesAroundHashParameters
+        Mocha::Configuration.override(:strict_keyword_argument_matching => true) do
+          mock.method(1, { :key => 42 }) # rubocop:disable Style/BracesAroundHashParameters
+        end
+      end
+      assert_equal [
+        'unexpected invocation: #<Mock:mock>.method(1, {:key => 42})',
+        'unsatisfied expectations:',
+        '- expected exactly once, invoked never: #<Mock:mock>.method(1, {:foo => 42})'
+      ], test_result.failure_message_lines
+    end
+
+    def test_unexpected_keyword_arguments_with_other_positionals_in_invocation_and_expectation_print_correctly_when_strict_keyword_args_is_enabled
+      test_result = run_as_test do
+        mock = mock('mock')
+        mock.expects(:method).with(1, :foo => 42)
+        Mocha::Configuration.override(:strict_keyword_argument_matching => true) do
+          mock.method(1, :key => 42)
+        end
+      end
+      assert_equal [
+        'unexpected invocation: #<Mock:mock>.method(1, :key => 42)',
+        'unsatisfied expectations:',
+        '- expected exactly once, invoked never: #<Mock:mock>.method(1, :foo => 42)'
+      ], test_result.failure_message_lines
+    end
   end
 end
