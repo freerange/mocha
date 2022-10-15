27dd3817ce1152ab1d93eed680e12b55c2f09fab
James Mead <james@floehopper.org>
2022-10-15 17:50:15 +0100

    More consistent Test::Unit & Minitest integration
    Use the value returned by Detection::TestUnit.testcase as the target of
    the TestUnit::Adapter module inclusion as in
    Mocha::Integration::Minitest.activate.

--- lib/mocha/integration/test_unit.rb
+++ lib/mocha/integration/test_unit.rb
@@ -7,18 +7,19 @@ module Mocha
   module Integration
     module TestUnit
       def self.activate
-        return false unless Detection::TestUnit.testcase
-        test_unit_version = Gem::Version.new(Detection::TestUnit.version)
+        target = Detection::TestUnit.testcase
+        return false unless target
 
+        test_unit_version = Gem::Version.new(Detection::TestUnit.version)
         Debug.puts "Detected Test::Unit version: #{test_unit_version}"
 
         unless TestUnit::Adapter.applicable_to?(test_unit_version)
           raise 'Versions of test-unit earlier than v2.5.1 are not supported.'
         end
 
-        unless ::Test::Unit::TestCase < TestUnit::Adapter
+        unless target < TestUnit::Adapter
           Debug.puts "Applying #{TestUnit::Adapter.description}"
-          ::Test::Unit::TestCase.send(:include, TestUnit::Adapter)
+          target.send(:include, TestUnit::Adapter)
         end
 
         true
