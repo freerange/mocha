9020f2090a61d6459022608edc91f3de39732dc3
Herwin Weststrate <herwin@snt.utwente.nl>
2022-12-25 20:38:33 +0100

    Rubocop: resolve Style/EachWithObject
    Ruby 1.9 is the lowest supported version, so this is always available.
    
    But the actual implementation was using a Hash lookup for the keys, but
    ignored the values. Set is a better datatype for this.

--- lib/mocha/debug.rb
+++ lib/mocha/debug.rb
@@ -1,12 +1,11 @@
+require 'set'
+
 module Mocha
   module Debug
-    OPTIONS = (ENV['MOCHA_OPTIONS'] || '').split(',').inject({}) do |hash, key|
-      hash[key] = true
-      hash
-    end.freeze
+    OPTIONS = (ENV['MOCHA_OPTIONS'] || '').split(',').to_set.freeze
 
     def self.puts(message)
-      warn(message) if OPTIONS['debug']
+      warn(message) if OPTIONS.include?('debug')
     end
   end
 end
