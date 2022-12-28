089a688e0bd5a6f171c1fc914d8c858c72c32221
Herwin Weststrate <herwin@snt.utwente.nl>
2022-12-28 13:27:23 +0100

    Rubocop: enable Style/FormatStringToken cop
    Since support for Ruby 1.8 has been dropped.

--- lib/mocha/names.rb
+++ lib/mocha/names.rb
@@ -37,7 +37,7 @@ module Mocha
     def mocha_inspect
       address = @mock.__id__ * 2
       address += 0x100000000 if address < 0
-      "#<Mock:0x#{format('%x', address)}>"
+      "#<Mock:0x#{format('%<address>x', address: address)}>"
     end
   end
 end
