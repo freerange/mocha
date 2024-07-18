96a85ad7ea5bf433d80575103b281dc21afc8f96
James Mead <james@floehopper.org>
2024-07-18 16:25:57 +0100

    Set top_level to true in PositionalOrKeywordHashTest
    These tests were all originally written with the `top_level: true`
    behaviour in mind and so I think it's better to make this explicit.

--- test/unit/parameter_matchers/positional_or_keyword_hash_test.rb
+++ test/unit/parameter_matchers/positional_or_keyword_hash_test.rb
@@ -11,27 +11,27 @@ class PositionalOrKeywordHashTest < Mocha::TestCase
   include Mocha::ParameterMatchers
 
   def test_should_describe_matcher
-    matcher = { key_1: 1, key_2: 2 }.to_matcher
+    matcher = { key_1: 1, key_2: 2 }.to_matcher(top_level: true)
     assert_equal '{:key_1 => 1, :key_2 => 2}', matcher.mocha_inspect
   end
 
   def test_should_match_non_last_hash_arg_with_hash_arg
-    matcher = { key_1: 1, key_2: 2 }.to_matcher
+    matcher = { key_1: 1, key_2: 2 }.to_matcher(top_level: true)
     assert matcher.matches?([{ key_1: 1, key_2: 2 }, %w[a b]])
   end
 
   def test_should_not_match_non_hash_arg_with_hash_arg
-    matcher = { key_1: 1, key_2: 2 }.to_matcher
+    matcher = { key_1: 1, key_2: 2 }.to_matcher(top_level: true)
     assert !matcher.matches?([%w[a b]])
   end
 
   def test_should_match_hash_arg_with_hash_arg
-    matcher = { key_1: 1, key_2: 2 }.to_matcher
+    matcher = { key_1: 1, key_2: 2 }.to_matcher(top_level: true)
     assert matcher.matches?([{ key_1: 1, key_2: 2 }])
   end
 
   def test_should_match_keyword_args_with_keyword_args
-    matcher = Hash.ruby2_keywords_hash({ key_1: 1, key_2: 2 }).to_matcher # rubocop:disable Style/BracesAroundHashParameters
+    matcher = Hash.ruby2_keywords_hash({ key_1: 1, key_2: 2 }).to_matcher(top_level: true) # rubocop:disable Style/BracesAroundHashParameters
     assert matcher.matches?([Hash.ruby2_keywords_hash({ key_1: 1, key_2: 2 })]) # rubocop:disable Style/BracesAroundHashParameters
   end
 
@@ -74,28 +74,28 @@ class PositionalOrKeywordHashTest < Mocha::TestCase
 
   if Mocha::RUBY_V27_PLUS
     def test_should_match_non_last_hash_arg_with_hash_arg_when_strict_keyword_args_is_enabled
-      matcher = { key_1: 1, key_2: 2 }.to_matcher
+      matcher = { key_1: 1, key_2: 2 }.to_matcher(top_level: true)
       Mocha::Configuration.override(strict_keyword_argument_matching: true) do
         assert matcher.matches?([{ key_1: 1, key_2: 2 }, %w[a b]])
       end
     end
 
     def test_should_not_match_non_hash_arg_with_hash_arg_when_strict_keyword_args_is_enabled
-      matcher = { key_1: 1, key_2: 2 }.to_matcher
+      matcher = { key_1: 1, key_2: 2 }.to_matcher(top_level: true)
       Mocha::Configuration.override(strict_keyword_argument_matching: true) do
         assert !matcher.matches?([%w[a b]])
       end
     end
 
     def test_should_match_hash_arg_with_hash_arg_when_strict_keyword_args_is_enabled
-      matcher = { key_1: 1, key_2: 2 }.to_matcher
+      matcher = { key_1: 1, key_2: 2 }.to_matcher(top_level: true)
       Mocha::Configuration.override(strict_keyword_argument_matching: true) do
         assert matcher.matches?([{ key_1: 1, key_2: 2 }])
       end
     end
 
     def test_should_match_keyword_args_with_keyword_args_when_strict_keyword_args_is_enabled
-      matcher = Hash.ruby2_keywords_hash({ key_1: 1, key_2: 2 }).to_matcher # rubocop:disable Style/BracesAroundHashParameters
+      matcher = Hash.ruby2_keywords_hash({ key_1: 1, key_2: 2 }).to_matcher(top_level: true) # rubocop:disable Style/BracesAroundHashParameters
       Mocha::Configuration.override(strict_keyword_argument_matching: true) do
         assert matcher.matches?([Hash.ruby2_keywords_hash({ key_1: 1, key_2: 2 })]) # rubocop:disable Style/BracesAroundHashParameters
       end
