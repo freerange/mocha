freerange/mocha

commit e9de64e493508457163a060220d3052d031af11f

Consolidate to_matcher definitions in one place

lib/mocha/parameter_matchers/instance_methods.rb
@@ -1,3 +1,4 @@
+require 'mocha/parameter_matchers/base'
 require 'mocha/parameter_matchers/equals'
 require 'mocha/parameter_matchers/positional_or_keyword_hash'
 
@@ -7,7 +8,9 @@ module Mocha
     module InstanceMethods
       # @private
       def to_matcher(expectation = nil, top_level = false)
-        if is_a?(Hash) && top_level
+        if is_a?(Base)
+          self
+        elsif is_a?(Hash) && top_level
           Mocha::ParameterMatchers::PositionalOrKeywordHash.new(self, expectation)
         else
           Mocha::ParameterMatchers::Equals.new(self)

lib/mocha/parameter_matchers/base.rb
@@ -2,11 +2,6 @@ module Mocha
   module ParameterMatchers
     # @abstract Subclass and implement +#matches?+ and +#mocha_inspect+ to define a custom matcher. Also add a suitably named instance method to {ParameterMatchers} to build an instance of the new matcher c.f. {#equals}.
     class Base
-      # @private
-      def to_matcher(_expectation = nil, _top_level = false)
-        self
-      end
-
       # A shorthand way of combining two matchers when both must match.
       #
       # Returns a new {AllOf} parameter matcher combining two matchers using a logical AND.

commit 7893c4bbebef1515af2a9ff9a7b6cdc19ce67cf9

Don't trust `Object#is_a?` in presence of Mocks
e9de64e493508457163a060220d3052d031af11f broke quite a few of our tests
because since we use Sorbet, some of our mocks have to stub `is_a?`
to comply with runtime type checking.

To be fair, our stubs were incorrect and after fixing them it's fine
but the failure was happening deep in Mocha and I think it could
be handled better.

Here's a small repro of the problem we ran into:

```ruby

require "bundler/inline"

gemfile(true) do
  source "https://rubygems.org"

  gem "minitest"
  gem "mocha"
end

require "minitest/autorun"
require "mocha/minitest"

module TypedAPI
  def self.call(user)
    raise TypeError, "Not a user" unless user.is_a?(User)
  end
end

class User
end

class BugTest < Minitest::Test
  def test_expect_mock
    user = mock("user")
    user.responds_like_instance_of(User)
    # This is wrong and shouldn't even work, but somehow it did
    # until now.
    user.stubs(:is_a? => User)
    TypedAPI.expects(:something).with(user)
    TypedAPI.call(user)
    TypedAPI.something(user)
  end
end
```

```
NoMethodError: undefined method `matches?' for #<Mock:user> which responds like #<User:0xde8>
    mocha-2.4.1/lib/mocha/mock.rb:389:in `check_responder_responds_to'
    mocha-2.4.1/lib/mocha/mock.rb:322:in `handle_method_call'
    mocha-2.4.1/lib/mocha/mock.rb:315:in `method_missing'
    mocha-2.4.1/lib/mocha/parameters_matcher.rb:21:in `block in parameters_match?'
    mocha-2.4.1/lib/mocha/parameters_matcher.rb:21:in `all?'
    mocha-2.4.1/lib/mocha/parameters_matcher.rb:21:in `parameters_match?'
    mocha-2.4.1/lib/mocha/parameters_matcher.rb:16:in `match?'
    mocha-2.4.1/lib/mocha/expectation.rb:647:in `match?'
    mocha-2.4.1/lib/mocha/expectation_list.rb:59:in `block in matching_expectations'
    mocha-2.4.1/lib/mocha/expectation_list.rb:59:in `select'
    mocha-2.4.1/lib/mocha/expectation_list.rb:59:in `matching_expectations'
    mocha-2.4.1/lib/mocha/expectation_list.rb:29:in `match_allowing_invocation'

.rubocop.yml
@@ -36,6 +36,11 @@ Style/WhileUntilModifier:
 Style/AccessModifierDeclarations:
   Enabled: false
 
+# `Module#===` is useful in presence of objects such as mocks
+# that may have a `is_a?` implementation that lies.
+Style/CaseEquality:
+  Enabled: false
+
 # This is useful when using `ExecutionPoint.current` to make tests more robust
 Style/Semicolon:
   Enabled: false

lib/mocha/parameter_matchers/instance_methods.rb
@@ -8,9 +8,9 @@ module Mocha
     module InstanceMethods
       # @private
       def to_matcher(expectation: nil, top_level: false)
-        if is_a?(Base)
+        if Base === self
           self
-        elsif is_a?(Hash) && top_level
+        elsif Hash === self && top_level
           Mocha::ParameterMatchers::PositionalOrKeywordHash.new(self, expectation)
         else
           Mocha::ParameterMatchers::Equals.new(self)