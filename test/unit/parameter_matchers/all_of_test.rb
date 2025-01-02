f49e59d398a9ed64e7ed8278b1326883af4c10cd
James Mead <james@floehopper.org>
2025-01-02 22:41:58 +0000

    Rename Stub::Matcher -> StubMatcher
    To match filename.

--- test/unit/parameter_matchers/all_of_test.rb
+++ test/unit/parameter_matchers/all_of_test.rb
@@ -10,17 +10,17 @@ class AllOfTest < Mocha::TestCase
   include Mocha::ParameterMatchers::Methods
 
   def test_should_match_if_all_matchers_match
-    matcher = all_of(Stub::Matcher.new(true), Stub::Matcher.new(true), Stub::Matcher.new(true))
+    matcher = all_of(StubMatcher.new(true), StubMatcher.new(true), StubMatcher.new(true))
     assert matcher.matches?(['any_old_value'])
   end
 
   def test_should_not_match_if_any_matcher_does_not_match
-    matcher = all_of(Stub::Matcher.new(true), Stub::Matcher.new(false), Stub::Matcher.new(true))
+    matcher = all_of(StubMatcher.new(true), StubMatcher.new(false), StubMatcher.new(true))
     assert !matcher.matches?(['any_old_value'])
   end
 
   def test_should_describe_matcher
-    matcher = all_of(Stub::Matcher.new(true), Stub::Matcher.new(false), Stub::Matcher.new(true))
+    matcher = all_of(StubMatcher.new(true), StubMatcher.new(false), StubMatcher.new(true))
     assert_equal 'all_of(matcher(true), matcher(false), matcher(true))', matcher.mocha_inspect
   end
 end
--- test/unit/parameter_matchers/not_test.rb
+++ test/unit/parameter_matchers/not_test.rb
@@ -10,17 +10,17 @@ class NotTest < Mocha::TestCase
   include Mocha::ParameterMatchers::Methods
 
   def test_should_match_if_matcher_does_not_match
-    matcher = Not(Stub::Matcher.new(false))
+    matcher = Not(StubMatcher.new(false))
     assert matcher.matches?(['any_old_value'])
   end
 
   def test_should_not_match_if_matcher_does_match
-    matcher = Not(Stub::Matcher.new(true))
+    matcher = Not(StubMatcher.new(true))
     assert !matcher.matches?(['any_old_value'])
   end
 
   def test_should_describe_matcher
-    matcher = Not(Stub::Matcher.new(true))
+    matcher = Not(StubMatcher.new(true))
     assert_equal 'Not(matcher(true))', matcher.mocha_inspect
   end
 end
