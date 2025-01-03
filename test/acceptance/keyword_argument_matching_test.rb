44b1f86d6bdf56ad933f0b28ba7e71a2f67a8c84
James Mead <james@floehopper.org>
2025-01-03 17:22:15 +0000

    Separate test cases in KeywordArgumentMatchingTest
    I've moved tests where loose keyword argument matching is explicitly
    disabled into `LooseKeywordArgumentMatchingTest` and tests where strict
    keyword argument matching is explicitly enabled into
    `StrictKeywordArgumentMatchingTest`. I'm hoping that this will allow me
    to make further simplifications to make the tests more readable.

--- test/acceptance/keyword_argument_matching_test.rb
+++ test/acceptance/keyword_argument_matching_test.rb
@@ -16,72 +16,6 @@ class KeywordArgumentMatchingTest < Mocha::TestCase
     teardown_acceptance_test
   end
 
-  def test_should_match_hash_parameter_with_keyword_args
-    execution_point = nil
-    test_result = run_as_test do
-      mock = mock()
-      mock.expects(:method).with(key: 42); execution_point = ExecutionPoint.current
-      Mocha::Configuration.override(strict_keyword_argument_matching: false) do
-        capture_deprecation_warnings do
-          mock.method({ key: 42 })
-        end
-      end
-    end
-    if Mocha::RUBY_V27_PLUS
-      location = execution_point.location
-      assert_includes test_result.last_deprecation_warning, "Expectation defined at #{location} expected keyword arguments (key: 42)"
-      assert_includes test_result.last_deprecation_warning, 'but received positional hash ({key: 42})'
-    end
-    assert_passed(test_result)
-  end
-
-  if Mocha::RUBY_V27_PLUS
-    def test_should_not_match_hash_parameter_with_keyword_args_when_strict_keyword_matching_is_enabled
-      test_result = run_as_test do
-        mock = mock()
-        mock.expects(:method).with(key: 42)
-        Mocha::Configuration.override(strict_keyword_argument_matching: true) do
-          mock.method({ key: 42 })
-        end
-      end
-      assert_failed(test_result)
-    end
-  end
-
-  def test_should_match_hash_parameter_with_splatted_keyword_args
-    execution_point = nil
-    test_result = run_as_test do
-      mock = mock()
-      kwargs = { key: 42 }
-      mock.expects(:method).with(**kwargs); execution_point = ExecutionPoint.current
-      Mocha::Configuration.override(strict_keyword_argument_matching: false) do
-        capture_deprecation_warnings do
-          mock.method({ key: 42 })
-        end
-      end
-    end
-    if Mocha::RUBY_V27_PLUS
-      location = execution_point.location
-      assert_includes test_result.last_deprecation_warning, "Expectation defined at #{location} expected keyword arguments (key: 42)"
-      assert_includes test_result.last_deprecation_warning, 'but received positional hash ({key: 42})'
-    end
-    assert_passed(test_result)
-  end
-
-  if Mocha::RUBY_V27_PLUS
-    def test_should_not_match_hash_parameter_with_splatted_keyword_args_when_strict_keyword_matching_is_enabled
-      test_result = run_as_test do
-        mock = mock()
-        kwargs = { key: 42 }
-        mock.expects(:method).with(**kwargs)
-        Mocha::Configuration.override(strict_keyword_argument_matching: true) do
-          mock.method({ key: 42 })
-        end
-      end
-      assert_failed(test_result)
-    end
-  end
-
   def test_should_match_splatted_hash_parameter_with_keyword_args
     test_result = run_as_test do
       mock = mock()
@@ -100,70 +34,6 @@ class KeywordArgumentMatchingTest < Mocha::TestCase
       mock.method(**kwargs)
     end
     assert_passed(test_result)
-  end
-
-  def test_should_match_positional_and_keyword_args_with_last_positional_hash
-    execution_point = nil
-    test_result = run_as_test do
-      mock = mock()
-      mock.expects(:method).with(1, { key: 42 }); execution_point = ExecutionPoint.current
-      Mocha::Configuration.override(strict_keyword_argument_matching: false) do
-        capture_deprecation_warnings do
-          mock.method(1, key: 42)
-        end
-      end
-    end
-    if Mocha::RUBY_V27_PLUS
-      location = execution_point.location
-      assert_includes test_result.last_deprecation_warning, "Expectation defined at #{location} expected positional hash ({key: 42})"
-      assert_includes test_result.last_deprecation_warning, 'but received keyword arguments (key: 42)'
-    end
-    assert_passed(test_result)
-  end
-
-  if Mocha::RUBY_V27_PLUS
-    def test_should_not_match_positional_and_keyword_args_with_last_positional_hash_when_strict_keyword_args_is_enabled
-      test_result = run_as_test do
-        mock = mock()
-        mock.expects(:method).with(1, { key: 42 })
-        Mocha::Configuration.override(strict_keyword_argument_matching: true) do
-          mock.method(1, key: 42)
-        end
-      end
-      assert_failed(test_result)
-    end
-  end
-
-  def test_should_match_last_positional_hash_with_keyword_args
-    execution_point = nil
-    test_result = run_as_test do
-      mock = mock()
-      mock.expects(:method).with(1, key: 42); execution_point = ExecutionPoint.current
-      Mocha::Configuration.override(strict_keyword_argument_matching: false) do
-        capture_deprecation_warnings do
-          mock.method(1, { key: 42 })
-        end
-      end
-    end
-    if Mocha::RUBY_V27_PLUS
-      location = execution_point.location
-      assert_includes test_result.last_deprecation_warning, "Expectation defined at #{location} expected keyword arguments (key: 42)"
-      assert_includes test_result.last_deprecation_warning, 'but received positional hash ({key: 42})'
-    end
-    assert_passed(test_result)
-  end
-
-  if Mocha::RUBY_V27_PLUS
-    def test_should_not_match_last_positional_hash_with_keyword_args_when_strict_keyword_args_is_enabled
-      test_result = run_as_test do
-        mock = mock()
-        mock.expects(:method).with(1, key: 42)
-        Mocha::Configuration.override(strict_keyword_argument_matching: true) do
-          mock.method(1, { key: 42 })
-        end
-      end
-      assert_failed(test_result)
-    end
   end
 
   def test_should_match_positional_and_keyword_args_with_keyword_args
@@ -237,6 +107,160 @@ class KeywordArgumentMatchingTest < Mocha::TestCase
       mock.method(1, { key: 42 })
     end
     assert_passed(test_result)
+  end
+end
+
+class LooseKeywordArgumentMatchingTest < Mocha::TestCase
+  include AcceptanceTestHelper
+
+  def setup
+    setup_acceptance_test
+  end
+
+  def teardown
+    teardown_acceptance_test
+  end
+
+  def test_should_match_hash_parameter_with_keyword_args
+    execution_point = nil
+    test_result = run_as_test do
+      mock = mock()
+      mock.expects(:method).with(key: 42); execution_point = ExecutionPoint.current
+      Mocha::Configuration.override(strict_keyword_argument_matching: false) do
+        capture_deprecation_warnings do
+          mock.method({ key: 42 })
+        end
+      end
+    end
+    if Mocha::RUBY_V27_PLUS
+      location = execution_point.location
+      assert_includes test_result.last_deprecation_warning, "Expectation defined at #{location} expected keyword arguments (key: 42)"
+      assert_includes test_result.last_deprecation_warning, 'but received positional hash ({key: 42})'
+    end
+    assert_passed(test_result)
+  end
+
+  def test_should_match_hash_parameter_with_splatted_keyword_args
+    execution_point = nil
+    test_result = run_as_test do
+      mock = mock()
+      kwargs = { key: 42 }
+      mock.expects(:method).with(**kwargs); execution_point = ExecutionPoint.current
+      Mocha::Configuration.override(strict_keyword_argument_matching: false) do
+        capture_deprecation_warnings do
+          mock.method({ key: 42 })
+        end
+      end
+    end
+    if Mocha::RUBY_V27_PLUS
+      location = execution_point.location
+      assert_includes test_result.last_deprecation_warning, "Expectation defined at #{location} expected keyword arguments (key: 42)"
+      assert_includes test_result.last_deprecation_warning, 'but received positional hash ({key: 42})'
+    end
+    assert_passed(test_result)
+  end
+
+  def test_should_match_positional_and_keyword_args_with_last_positional_hash
+    execution_point = nil
+    test_result = run_as_test do
+      mock = mock()
+      mock.expects(:method).with(1, { key: 42 }); execution_point = ExecutionPoint.current
+      Mocha::Configuration.override(strict_keyword_argument_matching: false) do
+        capture_deprecation_warnings do
+          mock.method(1, key: 42)
+        end
+      end
+    end
+    if Mocha::RUBY_V27_PLUS
+      location = execution_point.location
+      assert_includes test_result.last_deprecation_warning, "Expectation defined at #{location} expected positional hash ({key: 42})"
+      assert_includes test_result.last_deprecation_warning, 'but received keyword arguments (key: 42)'
+    end
+    assert_passed(test_result)
+  end
+
+  def test_should_match_last_positional_hash_with_keyword_args
+    execution_point = nil
+    test_result = run_as_test do
+      mock = mock()
+      mock.expects(:method).with(1, key: 42); execution_point = ExecutionPoint.current
+      Mocha::Configuration.override(strict_keyword_argument_matching: false) do
+        capture_deprecation_warnings do
+          mock.method(1, { key: 42 })
+        end
+      end
+    end
+    if Mocha::RUBY_V27_PLUS
+      location = execution_point.location
+      assert_includes test_result.last_deprecation_warning, "Expectation defined at #{location} expected keyword arguments (key: 42)"
+      assert_includes test_result.last_deprecation_warning, 'but received positional hash ({key: 42})'
+    end
+    assert_passed(test_result)
+  end
+end
+
+class StrictKeywordArgumentMatchingTest < Mocha::TestCase
+  include AcceptanceTestHelper
+
+  def setup
+    setup_acceptance_test
+  end
+
+  def teardown
+    teardown_acceptance_test
+  end
+
+  if Mocha::RUBY_V27_PLUS
+    def test_should_not_match_hash_parameter_with_keyword_args_when_strict_keyword_matching_is_enabled
+      test_result = run_as_test do
+        mock = mock()
+        mock.expects(:method).with(key: 42)
+        Mocha::Configuration.override(strict_keyword_argument_matching: true) do
+          mock.method({ key: 42 })
+        end
+      end
+      assert_failed(test_result)
+    end
+  end
+
+  if Mocha::RUBY_V27_PLUS
+    def test_should_not_match_hash_parameter_with_splatted_keyword_args_when_strict_keyword_matching_is_enabled
+      test_result = run_as_test do
+        mock = mock()
+        kwargs = { key: 42 }
+        mock.expects(:method).with(**kwargs)
+        Mocha::Configuration.override(strict_keyword_argument_matching: true) do
+          mock.method({ key: 42 })
+        end
+      end
+      assert_failed(test_result)
+    end
+  end
+
+  if Mocha::RUBY_V27_PLUS
+    def test_should_not_match_positional_and_keyword_args_with_last_positional_hash_when_strict_keyword_args_is_enabled
+      test_result = run_as_test do
+        mock = mock()
+        mock.expects(:method).with(1, { key: 42 })
+        Mocha::Configuration.override(strict_keyword_argument_matching: true) do
+          mock.method(1, key: 42)
+        end
+      end
+      assert_failed(test_result)
+    end
+  end
+
+  if Mocha::RUBY_V27_PLUS
+    def test_should_not_match_last_positional_hash_with_keyword_args_when_strict_keyword_args_is_enabled
+      test_result = run_as_test do
+        mock = mock()
+        mock.expects(:method).with(1, key: 42)
+        Mocha::Configuration.override(strict_keyword_argument_matching: true) do
+          mock.method(1, { key: 42 })
+        end
+      end
+      assert_failed(test_result)
+    end
   end
 
   if Mocha::RUBY_V27_PLUS
