73af73357ae20e2132b47a6a1493398454c6f9c6
James Mead <james@floehopper.org>
2026-02-15 15:55:54 +0000

    Fix test name in DeprecationTest

--- test/unit/deprecation_test.rb
+++ test/unit/deprecation_test.rb
@@ -6,7 +6,7 @@ require 'execution_point'
 require 'mocha/deprecation'
 
 class DeprecationTest < Minitest::Test
-  def test_foo
+  def test_output_deprecation_warning_to_stderr_with_source_location
     execution_point = nil
     _, stderr = capture_io do
       Mocha::Deprecation.warning('test-message'); execution_point = ExecutionPoint.current
